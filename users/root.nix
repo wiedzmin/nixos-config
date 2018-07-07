92665ae9245a66ac6a9e8e1502e8cf46ae83622a
Alex Ermolov <aaermolov@@gmail.com>
2018-07-07 12:27:02 +0300

    refactored users configuration

---
+++ users/root.nix
@@ -0,0 +1,6 @@
+{pkgs, ...}:
+{
+  imports = [
+  ];
+  users.extraUsers.root.hashedPassword = "586c56b7b6b6f68fca29c9ff2524e4dc52d51d5b6184a65f707dd3eae075e4c9afa81c9cd4042c26c9fb773d4f3de55fb55f363c6b0f5f6790baf4c4e3f32cb9";
+}
