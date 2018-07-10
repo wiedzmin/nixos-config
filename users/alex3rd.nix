8c17c560471d54704ad5c3b30cd17cb425f0d382
Alex Ermolov <aaermolov@@gmail.com>
2018-07-10 21:18:08 +0300

    users: reformatted alex3rd definitions

--- users/alex3rd.nix
+++ users/alex3rd.nix
@@ -1,36 +1,36 @@
 {pkgs, ...}:
 {
-  imports = [
-  ];
-  users.extraUsers = {
-      alex3rd = {
-          isNormalUser = true;
-          uid = 1000;
-          description = "Alex Ermolov";
-          shell = pkgs.zsh;
-          extraGroups = [ "audio" "docker" "lp" "networkmanager" "scanner" "vboxusers" "video" "wheel" ];
-      };
-  };
-  programs.zsh = {
-      enable = true;
-      ohMyZsh = {
-          enable = true;
-          plugins = [
-              "colored-man-pages"
-              "dirpersist"
-          ];
-          theme = "simple";
-      };
-      autosuggestions.enable = true;
-      enableCompletion = true;
-      interactiveShellInit = ''
-          PATH=$PATH:${pkgs.autojump}/bin
-          . ${pkgs.autojump}/share/autojump/autojump.zsh
-      '';
-  };
-  programs.zsh.zsh-autoenv = {
-      enable = true;
-      package = pkgs.zsh-autoenv;
-  };
-  programs.bash.enableCompletion = true;
+    imports = [
+    ];
+    users.extraUsers = {
+        alex3rd = {
+            isNormalUser = true;
+            uid = 1000;
+            description = "Alex Ermolov";
+            shell = pkgs.zsh;
+            extraGroups = [ "audio" "docker" "lp" "networkmanager" "scanner" "vboxusers" "video" "wheel" ];
+        };
+    };
+    programs.zsh = {
+        enable = true;
+        ohMyZsh = {
+            enable = true;
+            plugins = [
+                "colored-man-pages"
+                "dirpersist"
+            ];
+            theme = "simple";
+        };
+        autosuggestions.enable = true;
+        enableCompletion = true;
+        interactiveShellInit = ''
+            PATH=$PATH:${pkgs.autojump}/bin
+            . ${pkgs.autojump}/share/autojump/autojump.zsh
+        '';
+    };
+    programs.zsh.zsh-autoenv = {
+        enable = true;
+        package = pkgs.zsh-autoenv;
+    };
+    programs.bash.enableCompletion = true;
 }
