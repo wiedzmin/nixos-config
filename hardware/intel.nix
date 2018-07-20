e5f6a3304ce464dcc8292dd4b51445db96276738
Alex Ermolov <aaermolov@@gmail.com>
2018-07-20 22:07:21 +0300

    hardware: intel: removed obsolete line

--- hardware/intel.nix
+++ hardware/intel.nix
@@ -3,7 +3,6 @@
 {
     boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sdhci_pci" ];
     # boot.kernelModules = [ "kvm-intel" ];
-    boot.extraModulePackages = [ ];
     hardware.cpu.intel.updateMicrocode = true;
     hardware.enableAllFirmware = true;
     boot.extraModprobeConfig = ''
