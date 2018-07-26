06c4a75a00dea7f3d0f786eeb1cb73dc75fb78c5
Alex Ermolov <aaermolov@@gmail.com>
2018-07-26 18:56:31 +0300

    hardware: intel: added example for lates kernel selection

--- hardware/intel.nix
+++ hardware/intel.nix
@@ -5,6 +5,7 @@
     # boot.kernelModules = [ "kvm-intel" ];
     hardware.cpu.intel.updateMicrocode = true;
     hardware.enableAllFirmware = true;
+    # boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.extraModprobeConfig = ''
     #options iwlwifi 11n_disable=1 power_save=0
     '';
