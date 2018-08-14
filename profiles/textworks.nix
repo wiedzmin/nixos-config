c6bb73e315a5c249e92798e200ef10f5aa20d2ae
Alex Ermolov <aaermolov@@gmail.com>
2018-08-14 19:00:14 +0300

    profiles: textworks: added packages

--- profiles/textworks.nix
+++ profiles/textworks.nix
@@ -2,11 +2,17 @@
 
 {
     environment.systemPackages = with pkgs; [
+        # TODO: unoconv
         aspell
         aspellDicts.en
         aspellDicts.ru
         ditaa
+        djvu2pdf
+        djvulibre
+        ghostscript
+        mupdf
         pandoc
+        pdf2djvu
         pdf2djvu
         pdftk
         plantuml
