940dd84fc758590e511fa7f7ff2f8eaf19cb73df
Alex Ermolov <aaermolov@@gmail.com>
2018-09-07 12:38:47 +0300

    contrib: added script for updating nixpkgs
    It tags current HEAD with it's unixtime to create
    a sort of timestamp to be able to rollback to it
    in the future (if new HEAD is broken in some way,
    for example).

---
+++ contrib/update-nixpkgs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+set -x
+
+branch=nixos-unstable
+
+cd /etc/nixos/nixpkgs-channels
+git checkout $branch
+
+head_ts=`git show -s --format=%ct HEAD`
+git tag -a -s v_$head_ts -m "checkpoint at $head_ts"
+git fetch origin
+git rebase origin/$branch
