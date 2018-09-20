78c1a4a7d57cbb8b2e9eab737fe8523a5957ae8a
Alex Ermolov <aaermolov@@gmail.com>
2018-09-21 00:55:44 +0300

    hardware: misc: enabled EDID firmwares updating

--- hardware/misc.nix
+++ hardware/misc.nix
@@ -2,4 +2,13 @@
 
 {
     boot.extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
+    boot.kernelPatches = [
+        {
+            name = "edid-config";
+            patch = null;
+            extraConfig = ''
+                DRM_LOAD_EDID_FIRMWARE y
+          '';
+        }
+    ];
 }
