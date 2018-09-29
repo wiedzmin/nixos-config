47f8877df6eec4726727cf27f86a20cdc012c9ab
Alex Ermolov <aaermolov@@gmail.com>
2018-09-29 14:31:05 +0300

    hardware: misc: removed kernel flag for EDID loading
    It was added to be able to patch particular monitor EDID settings,
    (because of strange behaviour while being rotated by XRandR) but
    luckily workaround was found (being enabled, any custom kernel
    flags forces kernel rebuild on every nixpkgs update).

--- hardware/misc.nix
+++ hardware/misc.nix
@@ -2,13 +2,4 @@
 
 {
     boot.extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
-    boot.kernelPatches = [
-        {
-            name = "edid-config";
-            patch = null;
-            extraConfig = ''
-                DRM_LOAD_EDID_FIRMWARE y
-          '';
-        }
-    ];
 }
