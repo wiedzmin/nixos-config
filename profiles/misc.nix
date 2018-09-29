f68aaa80256f809789fc5778208ed216659a26df
Alex Ermolov <aaermolov@@gmail.com>
2018-09-29 14:28:41 +0300

    profiles: misc: disabled git-annex*
    Derivation became broken, will try to overlay.

--- profiles/misc.nix
+++ profiles/misc.nix
@@ -4,8 +4,8 @@
     environment.systemPackages = with pkgs; [
         appimage-run
         gcalcli
-        gitAndTools.git-annex
-        gitAndTools.git-annex-remote-rclone
+        # gitAndTools.git-annex # FIXME: try to overlay
+        # gitAndTools.git-annex-remote-rclone
         wirelesstools
         nrg2iso
         lsof # TODO: think of moving appropriately
