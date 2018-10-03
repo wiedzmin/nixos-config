2d1c042f46832665e7e9e765ac34a53400be3d16
Alex Ermolov <aaermolov@@gmail.com>
2018-10-04 01:16:04 +0300

    facets: misc: reenabled git-annex*, it was fixed

--- facets/misc.nix
+++ facets/misc.nix
@@ -4,8 +4,8 @@
     environment.systemPackages = with pkgs; [
         appimage-run
         gcalcli
-        # gitAndTools.git-annex # FIXME: try to overlay
-        # gitAndTools.git-annex-remote-rclone
+        gitAndTools.git-annex
+        gitAndTools.git-annex-remote-rclone
         wirelesstools
         lsof # TODO: think of moving appropriately
     ];
