0b51381e5d634aa32364e5dde62e35a0864db224
Alex Ermolov <aaermolov@@gmail.com>
2018-10-03 13:41:05 +0300

    contrib: automated home-manager updates

---
+++ contrib/update-home-manager.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+set -x
+
+branch=master
+
+cd /etc/nixos/home-manager/
+git checkout $branch
+
+git fetch origin
+git rebase origin/$branch
+head_ts=`git show -s --format=%ct HEAD`
+git tag -a -s v_$head_ts -m "checkpoint at $head_ts"
