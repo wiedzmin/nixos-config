9c2f822b3ce115b6303b02fa128ed394632c6253
Alex Ermolov <aaermolov@@gmail.com>
2018-10-13 20:52:38 +0300

    hardware: laptop: added list of services to restart after resume

--- hardware/laptop.nix
+++ hardware/laptop.nix
@@ -1,8 +1,14 @@
-{ lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
+    imports = [
+        ../private/hometraits.nix
+    ];
     powerManagement = {
         enable = true;
+        resumeCommands = (lib.concatMapStrings
+                          (service: "${pkgs.systemd}/bin/systemctl try-restart ${service}\n")
+                          config.sys.services_to_survive_suspend);
     };
     services.tlp.enable = true;
     services.acpid.enable = true;
