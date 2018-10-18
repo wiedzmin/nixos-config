6fb05f820f53834ab84210210d39f7617ad4bfd6
Alex Ermolov <aaermolov@@gmail.com>
2018-10-18 15:06:15 +0300

    services: xserver: added setup for xautolock

--- services/xserver/common.nix
+++ services/xserver/common.nix
@@ -45,6 +45,16 @@
             tappingDragLock = false;
             accelSpeed = "0.6";
         };
+        xautolock = {
+            enable = true;
+            enableNotifier = true;
+            time = 5;
+            locker = "${pkgs.i3lock-color}/bin/i3lock-color -c 232729; ${pkgs.xorg.xset}/bin/xset dpms force off";
+            nowlocker = "${pkgs.i3lock-color}/bin/i3lock-color -c 232729; ${pkgs.xorg.xset}/bin/xset dpms force off";
+            notify = 20;
+            notifier = "${pkgs.libnotify}/bin/notify-send -u critical \"Locking in 10 seconds\"";
+            extraOptions = ["-lockaftersleep" "-detectsleep"];
+        };
     };
 
     services.arbtt.enable = true;
