384b4dd3a70fe6abedb9fdfb3987c26160bf4e5f
Alex Ermolov <aaermolov@@gmail.com>
2018-10-22 14:16:16 +0300

    services: xorg: updated xautolock setup
    - removed notifier, it is generally not that useful,
      but keeps displaying untimely notifications, even
      with timeout. May be tried again later.
    - finally, lock screen after resume, it makes sense
      in some situations.

--- services/xorg/server.nix
+++ services/xorg/server.nix
@@ -47,13 +47,10 @@
         };
         xautolock = {
             enable = true;
-            enableNotifier = true;
-            time = 5;
+            time = 3;
             locker = "${pkgs.xkblayout-state}/bin/xkblayout-state set 0; ${pkgs.i3lock-color}/bin/i3lock-color -c 232729; ${pkgs.xorg.xset}/bin/xset dpms force off";
             nowlocker = "${pkgs.xkblayout-state}/bin/xkblayout-state set 0; ${pkgs.i3lock-color}/bin/i3lock-color -c 232729; ${pkgs.xorg.xset}/bin/xset dpms force off";
-            notify = 20;
-            notifier = "${pkgs.libnotify}/bin/notify-send -t 5000 -u critical \"Locking in 10 seconds\"";
-            extraOptions = ["-detectsleep"];
+            extraOptions = ["-detectsleep" "-lockaftersleep"];
         };
     };
 
