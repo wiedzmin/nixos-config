8a7096ca86d77a1f9c5133333ee3fb3e28cc4f8a
Alex Ermolov <aaermolov@@gmail.com>
2018-11-12 00:27:16 +0300

    users: alex3rd: zsh: removed enhancd plugin
    It is not that useful to allow conflicting
    with more versatile tools.

--- users/alex3rd/dotfiles/shell/zsh.nix
+++ users/alex3rd/dotfiles/shell/zsh.nix
@@ -110,17 +110,6 @@ in
                     };
                 }
                 {
-                    name = "enhancd";
-                    file = "init.sh";
-                    src = pkgs.fetchFromGitHub {
-                        owner = "b4b4r07";
-                        repo = "enhancd";
-                        rev = "v2.2.1";
-                        sha256 = "0iqa9j09fwm6nj5rpip87x3hnvbbz9w9ajgm6wkrd5fls8fn8i5g";
-                    };
-                }
-                {
-                    # FIXME: no notifier program found
                     name = "zsh-notify";
                     file = "notify.plugin.zsh";
                     src = pkgs.fetchFromGitHub {
