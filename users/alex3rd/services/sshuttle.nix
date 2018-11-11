e3c896f0dc302d6b18bbbfa4fa61ba7bda232d47
Alex Ermolov <aaermolov@@gmail.com>
2018-11-11 22:40:59 +0300

    users: alex3rd: do not mark sshuttle service failed

--- users/alex3rd/services/sshuttle.nix
+++ users/alex3rd/services/sshuttle.nix
@@ -11,7 +11,7 @@
             Type = "forking";
             Restart = "always";
             RestartSec = 2;
-            ExecStart = "${pkgs.sshuttle}/bin/sshuttle -D --dns -r ${config.sshuttle_traits.remote}" +
+            ExecStart = "-${pkgs.sshuttle}/bin/sshuttle -D --dns -r ${config.sshuttle_traits.remote}" +
                         (lib.concatMapStrings (subnet: " -x ${subnet}") config.sshuttle_traits.exclude) +
                         " 0/0 --pidfile=/var/run/sshuttle.pid -e 'ssh -i ${config.sshuttle_traits.ssh_identity_file}'";
             ExecStartPost = "${pkgs.systemd}/bin/systemctl try-restart imapfilter.service"; # TODO: invent a global list of services that needs to be restarted this way
