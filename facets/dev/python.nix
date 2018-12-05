b18df01d9704dc74e533d2ab64b14662faf67e91
Alex Ermolov <aaermolov@@gmail.com>
2018-12-05 12:39:50 +0300

    dev: python: removed obsolete/extra packages
    Those are mostly being pulled as deps for first-class
    tools, such as PyLS or could be included into shell
    environments such some python libs needed for certain
    Ansible modules. The rest are mainly from pre-PyLS epoch.

--- facets/dev/python.nix
+++ facets/dev/python.nix
@@ -10,31 +10,12 @@
         # dev
         idea.pycharm-community
         pyre
-        python3Packages.GitPython
-        python3Packages.autopep8
-        python3Packages.black
-        python3Packages.flake8
-        python3Packages.importmagic
-        python3Packages.isort
-        python3Packages.jedi
-        python3Packages.jmespath
-        python3Packages.mccabe
-        python3Packages.mypy
         python3Packages.notebook
-        python3Packages.olefile
-        python3Packages.pep8
-        python3Packages.pycodestyle
-        python3Packages.pydocstyle
-        python3Packages.pyflakes
-        python3Packages.pylint
         python3Packages.pyls-black
         python3Packages.pyls-isort
         python3Packages.pyls-mypy
         python3Packages.python-language-server
-        python3Packages.rope
         python3Packages.snakeviz
-        python3Packages.virtualenv
         python3Packages.virtualenvwrapper
-        python3Packages.yapf
     ];
 }
