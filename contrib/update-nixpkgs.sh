db56bd754bfe1789200b1ea38c9003caa5a2d363
Alex Ermolov <aaermolov@@gmail.com>
2019-01-09 13:06:52 +0300

    contrib: added fallback functionality to update-nixpkgs.sh
    Some crucial software could be broken on nixpkgs/unstable, so
    there should be a way to go back to last working state.

--- contrib/update-nixpkgs.sh
+++ contrib/update-nixpkgs.sh
@@ -3,11 +3,17 @@ set -euo pipefail
 set -x
 
 branch=nixos-unstable
+fallback_branch=nixos-unstable-working
 
 cd /etc/nixos/pkgs/nixpkgs-channels
 git checkout $branch
+
+current_system_commit_hash=`readlink -f /run/current-system | cut -f4 -d.`
+git branch -f $fallback_branch $current_system_commit_hash
+git tag -a -s --force last_working -m "last nixpkgs built and working" $current_system_commit_hash
 
 git fetch origin
 git rebase origin/$branch
 head_ts=`git show -s --format=%ct HEAD`
 git tag -a -s v_$head_ts -m "checkpoint at $head_ts"
+git checkout $branch
