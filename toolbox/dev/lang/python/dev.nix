839d6cc854c75a85c3c450d9a0d7d1068f7f65d6
Alex Ermolov <aaermolov@@gmail.com>
2019-02-28 13:00:13 +0300

    dev: python: next fix of lsp/emacs setup
    - PyLS should be installed for both (2/3) python
      versions, with respective plugins, if any
    - linters, formatters and other auxillary tools
      should be installed to projects' venvs

--- toolbox/dev/lang/python/dev.nix
+++ toolbox/dev/lang/python/dev.nix
@@ -2,14 +2,12 @@
 
 {
     environment.systemPackages = with pkgs; [
-        (python3.withPackages (ps: with ps; [
-           python-language-server
-           python3Packages.pyls-black
-           python3Packages.pyls-isort
-           python3Packages.pyls-mypy
-        ]))
-        python3Packages.black
-        python3Packages.pylama
+        python2Packages.python-language-server
+        python2Packages.pyls-isort
+        python3Packages.python-language-server
+        python3Packages.pyls-black
+        python3Packages.pyls-isort
+        python3Packages.pyls-mypy
         python3Packages.snakeviz
         python3Packages.virtualenvwrapper
     ];
