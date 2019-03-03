b1e2d45fc4bb717f0c6a53de3fde26e5daf6734f
Alex Ermolov <aaermolov@@gmail.com>
2019-03-03 18:25:05 +0300

    dev: python: re-sorted packages list

--- toolbox/dev/lang/python/dev.nix
+++ toolbox/dev/lang/python/dev.nix
@@ -2,11 +2,11 @@
 
 {
     environment.systemPackages = with pkgs; [
-        python2Packages.python-language-server
         python2Packages.pyls-isort
-        python3Packages.python-language-server
+        python2Packages.python-language-server
         python3Packages.pyls-isort
         python3Packages.pyls-mypy
+        python3Packages.python-language-server
         python3Packages.snakeviz
         python3Packages.virtualenvwrapper
         yapf
