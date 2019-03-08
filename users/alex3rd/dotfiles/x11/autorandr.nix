b43377350124456f1768144edaab070537901413
Alex Ermolov <aaermolov@@gmail.com>
2019-03-09 01:01:44 +0300

    hardware: laptoptop: actualized autorandr profiles

--- users/alex3rd/dotfiles/x11/autorandr.nix
+++ users/alex3rd/dotfiles/x11/autorandr.nix
@@ -17,13 +17,14 @@
                     ${pkgs.feh}/bin/feh --bg-fill ${config.sys.wallpaper.baseDir}/${config.sys.wallpaper.current}
                 '';
             };
-            # TODO: actualize list below
-            ".config/autorandr/mobile/config".source = ../../../../hardware/autorandr/x230/mobile/config;
-            ".config/autorandr/mobile/setup".source = ../../../../hardware/autorandr/x230/mobile/setup;
-            ".config/autorandr/docked-home/config".source = ../../../../hardware/autorandr/x230/docked-home/config;
-            ".config/autorandr/docked-home/setup".source = ../../../../hardware/autorandr/x230/docked-home/setup;
-            ".config/autorandr/docked-office-double/config".source = ../../../../hardware/autorandr/x230/docked-office-double/config;
-            ".config/autorandr/docked-office-double/setup".source = ../../../../hardware/autorandr/x230/docked-office-double/setup;
+            ".config/autorandr/docked-home/config".source = ../../../../hardware/autorandr/laptoptop/docked-home/config;
+            ".config/autorandr/docked-home/setup".source = ../../../../hardware/autorandr/laptoptop/docked-home/setup;
+            ".config/autorandr/docked-office/config".source = ../../../../hardware/autorandr/laptoptop/docked-office/config;
+            ".config/autorandr/docked-office/setup".source = ../../../../hardware/autorandr/laptoptop/docked-office/setup;
+            ".config/autorandr/mobile/config".source = ../../../../hardware/autorandr/laptoptop/mobile/config;
+            ".config/autorandr/mobile/setup".source = ../../../../hardware/autorandr/laptoptop/mobile/setup;
+            ".config/autorandr/undocked-parents-dsub/config".source = ../../../../hardware/autorandr/laptoptop/undocked-parents-dsub/config;
+            ".config/autorandr/undocked-parents-dsub/setup".source = ../../../../hardware/autorandr/laptoptop/undocked-parents-dsub/setup;
         };
     };
 }
