0f230db9bc751c306a70dd8386dc854e05eafa93
Alex Ermolov <aaermolov@@gmail.com>
2019-03-23 16:37:37 +0300

    staging: added packages

--- toolbox/misc/staging.nix
+++ toolbox/misc/staging.nix
@@ -10,11 +10,14 @@
         # pyflame
         bbe
         clerk
+        findnewest
+        git-town
         gitAndTools.git-ignore
         gitAndTools.git-imerge
         jsonnet
         k6
         nix-prefetch
         sysdig
+        websocat
     ];
 }
