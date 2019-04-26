41c48a045c39417b5aafd7c91678d36b20a25092
Alex Ermolov <aaermolov@@gmail.com>
2019-04-26 20:48:55 +0300

    users: alex3rd: emacs: do not nullify file-name-handler-alist
    They're not speeding up startup too much, but instead break encrypted
    credentials handling anyway.

--- users/alex3rd/dotfiles/dev/emacs/default.nix
+++ users/alex3rd/dotfiles/dev/emacs/default.nix
@@ -336,24 +336,17 @@
                 (setq message-log-max t) ;; we don't want to lose any startup log info
                 (setq shell-file-name "${pkgs.bash}/bin/bash")
 
-
                 (setq gc-cons-percentage 0.3)
 
                 (setq gc-cons-threshold most-positive-fixnum)
 
-                (defvar temp/file-name-handler-alist file-name-handler-alist)
-
                 (add-hook 'after-init-hook #'(lambda ()
-                                               (setq gc-cons-threshold 800000)
-                                               (setq file-name-handler-alist temp/file-name-handler-alist)
-                                               ))
+                                               (setq gc-cons-threshold 800000)))
 
                 (add-hook 'minibuffer-setup-hook (lambda () (setq gc-cons-threshold most-positive-fixnum)))
                 (add-hook 'minibuffer-exit-hook (lambda () (setq gc-cons-threshold 800000)))
 
                 (add-hook 'focus-out-hook #'garbage-collect)
-
-                (setq file-name-handler-alist nil)
 
                 ${builtins.readFile ./bootstrap.el}
                 ${builtins.readFile ./base.el}
