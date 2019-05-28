c25f5ff2c2389da06f6876c2315aadd6650d9088
Alex Ermolov <aaermolov@@gmail.com>
2019-05-28 15:37:52 +0300

    machines: laptoptop: beautified systemPackages list

--- machines/laptoptop.nix
+++ machines/laptoptop.nix
@@ -59,16 +59,17 @@
 
     sound.enable = true;
 
-    environment.systemPackages = with pkgs; [
+    environment.systemPackages = with pkgs; with config.boot.kernelPackages; [
         pasystray
         pavucontrol
         # ocz-ssd-guru # add as an overlay and fix hash (and installation instructions)
         intelmetool
         me_cleaner
         smartmontools
-        config.boot.kernelPackages.perf
+    ] ++ [
+        perf
+        cpupower
     ];
-
     powerManagement = {
         enable = true;
         powertop.enable = true;
