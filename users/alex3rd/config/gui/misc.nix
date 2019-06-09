2fbf6137cb277387cb3d8a0c4905066b2baba652
Alex Ermolov <aaermolov@@gmail.com>
2019-06-09 19:34:25 +0300

    users: alex3rd: gui: removed xmodmaprc as standalone file

--- users/alex3rd/config/gui/misc.nix
+++ users/alex3rd/config/gui/misc.nix
@@ -7,19 +7,6 @@ with import ../../const.nix {inherit config pkgs;};
         ../../private/nas.nix
     ];
 
-    environment.etc."Xmodmaprc".text = ''
-        clear mod1
-        clear mod4
-        clear mod5
-        keycode 64 = Alt_L Meta_L
-        keycode 133 = Super_L
-        keycode 108 = Hyper_L
-        keycode 191 = Insert
-        add mod1 = Meta_L
-        add mod1 = Alt_L
-        add mod4 = Super_L
-        add mod5 = Hyper_L
-    '';
     home-manager.users."${userName}" = {
         home.file = {
             "${config.common.snippets.file}".text = ''
--- users/alex3rd/config/gui/xserver.nix
+++ users/alex3rd/config/gui/xserver.nix
@@ -23,8 +23,23 @@ with import ../../const.nix {inherit config pkgs;};
                     };
                 };
                 gdm.enable = false;
-                sessionCommands = ''
-                    ${pkgs.xlibs.xmodmap}/bin/xmodmap /etc/Xmodmaprc
+                sessionCommands = let
+                    xmodmaprc = pkgs.writeText "xmodmaprc" ''
+                        clear mod1
+                        clear mod4
+                        clear mod5
+                        keycode 64 = Alt_L Meta_L
+                        keycode 133 = Super_L
+                        keycode 108 = Hyper_L
+                        keycode 191 = Insert
+                        add mod1 = Meta_L
+                        add mod1 = Alt_L
+                        add mod4 = Super_L
+                        add mod5 = Hyper_L
+                    '';
+                in
+                ''
+                    ${pkgs.xlibs.xmodmap}/bin/xmodmap ${xmodmaprc}
                     ${pkgs.xlibs.xmodmap}/bin/xmodmap -e "clear Lock"
 
                     export CURRENT_WM=${config.services.xserver.windowManager.default}
