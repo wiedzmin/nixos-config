ea4c733ea93129ece07bbd80f7235514fc064f51
Alex Ermolov <aaermolov@@gmail.com>
2019-06-12 00:20:11 +0300

    users: alex3rd: modules: brushed lowbatt service a bit

--- users/alex3rd/modules/lowbatt.nix
+++ users/alex3rd/modules/lowbatt.nix
@@ -9,27 +9,45 @@ in {
     options = {
         services.batteryNotifier = {
             enable = mkOption {
+                type = types.bool;
                 default = false;
                 description = ''
                     Whether to enable battery notifier.
                 '';
             };
             device = mkOption {
+                type = types.str;
                 default = "BAT0";
                 description = ''
                     Device to monitor.
                 '';
             };
             notifyCapacity = mkOption {
+                type = types.int;
                 default = 10;
                 description = ''
-                    Battery level at which a notification shall be sent.
+                    Battery level at which a notification should be sent.
                 '';
             };
             suspendCapacity = mkOption {
+                type = types.int;
                 default = 5;
                 description = ''
-                    Battery level at which a suspend unless connected shall be sent.
+                    Battery level at which a suspend should be performed.
+                '';
+            };
+            suspendTimeout = mkOption {
+                type = types.str;
+                default = "60s";
+                description = ''
+                    How much to wait before suspending.
+                '';
+            };
+            suspendCommand = mkOption {
+                type = types.str;
+                default = "systemctl suspend";
+                description = ''
+                    Command to issue on suspend.
                 '';
             };
         };
@@ -50,17 +68,16 @@ in {
                 export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/capacity)
                 export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/status)
                 if [[ $battery_capacity -le ${builtins.toString cfg.notifyCapacity} && $battery_status = "Discharging" ]]; then
-                    ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery Low" "You should probably plug-in."
+                    ${pkgs.dunst}/bin/dunstify -u critical "Battery Low, you should probably plug-in."
                 fi
 
                 if [[ $battery_capacity -le ${builtins.toString cfg.suspendCapacity} && $battery_status = "Discharging" ]]; then
-                    ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery Critically Low" "Computer will suspend in 60 seconds."
-                    sleep 60s
+                    ${pkgs.dunst}/bin/dunstify -u critical -t 5000 Battery CRITICALLY low, will suspend in ${cfg.suspendTimeout}."
+                    sleep ${cfg.suspendTimeout}
 
                     battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/status)
                     if [[ $battery_status = "Discharging" ]]; then
-                        systemctl suspend
-                        #systemctl poweroff
+                        ${cfg.suspendCommand}
                     fi
                 fi
             '';
