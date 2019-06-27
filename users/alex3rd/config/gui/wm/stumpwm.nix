8a1bf2908dc6d12ec8159084f76ccb0728e9d1d4
Alex Ermolov <aaermolov@@gmail.com>
2019-06-27 21:23:00 +0300

    users: alex3rd: gui: fetch stumpwm config when using stumpwm

--- users/alex3rd/config/gui/wm/stumpwm.nix
+++ users/alex3rd/config/gui/wm/stumpwm.nix
@@ -1,7 +1,18 @@
 { config, pkgs, ... }:
+with import ../../../const.nix {inherit config pkgs;};
 {
     services.xserver.windowManager = {
         default = "stumpwm";
         stumpwm.enable = true;
+    };
+    home-manager.users."${userName}" = {
+        home.file = {
+            ".stumpwm.d".source = pkgs.fetchFromGitHub {
+                owner = "wiedzmin";
+                repo = "stumpwm-config";
+                rev = "832513053bb70fc0d24620bc4607b0031193f526";
+                sha256 = "0yx3xnw33zkmgl92jnzvzqqvqb7q80gz9ysm3ngbba3hawn2vmma";
+            };
+        };
     };
 }
