0bf628e790d892f31d52369bea88984f50de94d9
Alex Ermolov <aaermolov@@gmail.com>
2019-09-21 15:06:37 +0300

    users: alex3rd: removed unused config

--- users/alex3rd/config/shell/common.nix
+++ users/alex3rd/config/shell/common.nix
@@ -4,68 +4,6 @@ with import ../../secrets/const.nix { inherit config pkgs lib; }; {
   imports = [ ../../secrets/job.nix ];
 
   home-manager.users."${userName}" = {
-    xdg.configFile."wtf/config.yml".text = ''
-      wtf:
-        colors:
-          border:
-            focusable: darkslateblue
-            focused: orange
-            normal: gray
-        grid:
-          columns: [40, 40]
-          rows: [13, 13, 4]
-        refreshInterval: 1
-        mods:
-          clocks:
-            colors:
-              rows:
-                even: "lightblue"
-                odd: "white"
-            enabled: true
-            locations:
-              ${builtins.concatStringsSep "\n        "
-      (lib.mapAttrsToList (caption: timezone: caption + ": " + timezone) worldClocks)}
-            position:
-              top: 0
-              left: 0
-              height: 1
-              width: 1
-            refreshInterval: 15
-            sort: "alphabetical"
-          security:
-            enabled: true
-            position:
-              top: 1
-              left: 0
-              height: 1
-              width: 1
-            refreshInterval: 3600
-          status:
-            enabled: true
-            position:
-              top: 2
-              left: 0
-              height: 1
-              width: 2
-            refreshInterval: 1
-          system:
-            enabled: true
-            position:
-              top: 0
-              left: 1
-              height: 1
-              width: 1
-            refreshInterval: 3600
-          textfile:
-            enabled: true
-            filePath: "~/.config/wtf/config.yml"
-            position:
-              top: 1
-              left: 1
-              height: 1
-              width: 1
-            refreshInterval: 30
-    '';
     programs.htop = {
       enable = true;
       fields = [ "USER" "PRIORITY" "NICE" "M_SIZE" "STATE" "PERCENT_CPU" "PERCENT_MEM" "TIME" "COMM" ];
