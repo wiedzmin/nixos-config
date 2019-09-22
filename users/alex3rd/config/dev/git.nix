a2542768698637c46c1271920d74792fbf839cfb
Alex Ermolov <aaermolov@@gmail.com>
2019-09-22 14:58:06 +0300

    users: alex3rd: git: reduced setup size

--- users/alex3rd/config/dev/git.nix
+++ users/alex3rd/config/dev/git.nix
@@ -57,94 +57,15 @@ in {
       } + ''
         include = cat ${jobWorkspacePath}/.mrconfig
       '';
-      "git-assets/git-commit-template".text = ''
-
-
-        # <type>: (If applied, this commit will...) <subject> (Max 50 char)
-        # |<----  Using a Maximum Of 50 Characters  ---->|
-
-        # Explain why this change is being made
-        # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
-
-        # Provide links or keys to any relevant tickets, articles or other resources
-        # Example: Github issue #23
-
-        # --- COMMIT END ---
-        # Type can be
-        #    feat     (new feature)
-        #    fix      (bug fix)
-        #    refactor (refactoring production code)
-        #    style    (formatting, missing semi colons, etc; no code change)
-        #    docs     (changes to documentation)
-        #    test     (adding or refactoring tests; no production code change)
-        #    chore    (updating grunt tasks etc; no production code change)
-        # --------------------
-        # Remember to
-        #    Capitalize the subject line
-        #    Use the imperative mood in the subject line
-        #    Do not end the subject line with a period
-        #    Separate subject from body with a blank line
-        #    Use the body to explain what and why vs. how
-        #    Can use multiple lines with "-" for bullet points in body
-        # --------------------
-        # For more information about this template, check out
-        # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50
-      '';
       "git-assets/.gitignore".text = ''
         # reference ignore list / inspired by Github
-        *.o
-        *.ko
-        *.so
-        *.obj
-        *.elf
-
-        *.lib
-        *.a
-        *.la
-        *.lo
-
-        *.dll
-        *.so
-        *.so.*
-        *.dylib
-
-        *.exe
-        *.out
-        *.app
-        *.i*86
-        #.x86_64
-        *.hex
-        *.dSYM/
-
         # emacs
-        *~
-        \#*\#
         *.elc
-        auto-save-list
-        .\#*
-
-        # Autotools
-        Makefile.in
-        /autom4te.cache
-        /aclocal.m4
-        /compile
-        /configure
-        /depcomp
-        /install-sh
-        /missing
-        /m4
-        /stamp-h1
-
-        # various
-        .DS_Store
-        *.out
-        /.DS_Store
-        *.swp
-        cache/*
-        page_cache/*
-        .agignore
         .dir-locals.el
 
+        # various
+        *.out
+        *.swp
         .mypy_cache/*
 
         ${gitRepoHooks}
@@ -215,7 +136,6 @@ in {
           changed = "yellow";
           untracked = "red";
         };
-        "commit" = { template = "/home/${userName}/git-assets/git-commit-template"; };
         "rebase" = {
           autoSquash = true;
           autoStash = true;
@@ -242,48 +162,24 @@ in {
         "github" = { user = "wiedzmin"; };
       };
       aliases = {
-        co = "checkout";
-        cwo = "checkout -b origin/"; # fetch branch from primary remote, eliminates ambiguity
-
         bl = "branch -l";
         merged = "branch --merged master";
         nomerged = "branch --no-merged master";
-        fro = "!f() { git fetch && git rebase '@{u}'}; f";
-
-        cs = "commit -S";
 
         undo = "reset HEAD~1";
         hundo = "reset --hard HEAD~1";
 
-        cont = "shortlog -n -s";
-        fc =
-          "!f() { git log --pretty=format:'* %C(yellow)%h%Creset -%C(red)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%cn>%Creset' --decorate --date=relative --grep=$1; }; f";
-        hist = ''log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'';
-        lg =
-          "log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
         who = "shortlog -n -s --no-merges";
         sl = "log --name-only --oneline";
-        updated = "show --name-only --oneline";
 
         # TODO: think of using --patience
-        d = "!git diff --patch-with-stat --color $@ | ${pkgs.gitAndTools.diff-so-fancy}/bin/diff-so-fancy";
-        dc = "diff --cached";
         df = "diff --patch-with-stat --color --color-words --abbrev";
-        fpd = "diff --no-prefix -s -p >";
         last = "diff --stat=150,120 HEAD^ HEAD";
-        pd = "diff --no-prefix -s -p >";
 
         pf = "format-patch -1 --no-prefix -s -p FETCH_HEAD";
 
-        s = "status -s";
-        st = "status";
-
         remotes = "remote -v";
-        sclone = "clone --depth 1";
-
-        trycl = "git clean -d -x -n -i";
-        cleanup = "clean -d -x -f";
-        tcleanup = "remote prune origin && git gc && git clean -dfx && git stash clear";
+        slice = "clone --depth 1";
       };
     };
   };
