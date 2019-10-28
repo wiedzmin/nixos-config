b7e9849462c2393ada4c198b096750f39b4262b5
Alex Ermolov <aaermolov@@gmail.com>
2019-10-28 09:37:09 +0300

    modules: reworked dbms script in python for new infrastructure

--- modules/common/dbms.nix
+++ modules/common/dbms.nix
@@ -3,48 +3,39 @@ with lib;
 
 let
   cfg = config.tools.dbms;
-  dbms = pkgs.writeShellScriptBin "dbms" ''
-    enforce_job_vpn_up || exit 1
+  dbms = pkgs.writeScriptBin "dbms" ''
+    #! /usr/bin/env nix-shell
+    #! nix-shell -i python3 -p python3 python3Packages.dmenu-python python3Packages.redis
+    import json
+    import os
+    import subprocess
 
-    declare -A DBMS_TRAITS
+    import dmenu
+    import redis
 
-    DBMS_TRAITS=(
-    ${builtins.concatStringsSep "\n" (lib.mapAttrsToList
-      (alias: meta: "  [\"${alias}\"]=\"${meta.ip} ${meta.command} ${meta.user} ${meta.passwordPassPath}\"")
-      (config.secrets.job.infra.dbmsMeta))}
-    )
 
-    MYCLI_BINARY=${pkgs.mypgWorking.mycli}/bin/mycli
-    PGCLI_BINARY=${pkgs.mypgWorking.pgcli}/bin/pgcli
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    dbms_meta = json.loads(r.get("job/dbms_meta"))
 
-    list_dbms_traits() {
-        for i in "''${!DBMS_TRAITS[@]}"
-        do
-            echo "$i"
-        done
-    }
+    dbms_entry = dmenu.show(dbms_meta.keys(), lines=5)
+    if dbms_entry:
+        dbms_pass_task = subprocess.Popen("${pkgs.pass}/bin/pass {0}".format(dbms_meta[dbms_entry]["passwordPassPath"]),
+                                          shell=True, stdout=subprocess.PIPE)
+        dbms_pass = dbms_pass_task.stdout.read().decode().split("\n")[0]
+        assert dbms_pass_task.wait() == 0
 
-    main() {
-        DBMS_META="''${DBMS_TRAITS[$( (list_dbms_traits) | ${pkgs.rofi}/bin/rofi -dmenu -p "Connect" )]}"
-        if [ -n "$DBMS_META" ]; then
-            DBMS_IP=$(echo $DBMS_META | ${pkgs.coreutils}/bin/cut -f1 -d\ )
-            DBMS_COMMAND=$(echo $DBMS_META | ${pkgs.coreutils}/bin/cut -f2 -d\ )
-            DBMS_USER=$(echo $DBMS_META | ${pkgs.coreutils}/bin/cut -f3 -d\ )
-            DBMS_PASSWORD_PASS_PATH=$(echo $DBMS_META | ${pkgs.coreutils}/bin/cut -f4 -d\ )
-            DBMS_PASSWORD=$(${pkgs.pass}/bin/pass $DBMS_PASSWORD_PASS_PATH)
-            CLI_BINARY_VARNAME="''${DBMS_COMMAND^^}_BINARY"
-            CLI_EXECUTABLE="''${!CLI_BINARY_VARNAME}"
-            if [ "$DBMS_COMMAND" == "mycli" ]; then
-                ${pkgs.tmux}/bin/tmux new-window "$CLI_EXECUTABLE --host $DBMS_IP --user $DBMS_USER --password $DBMS_PASSWORD"
-            elif [ "$DBMS_COMMAND" == "pgcli" ]; then
-                ${pkgs.tmux}/bin/tmux new-window "PGPASSWORD=$DBMS_PASSWORD $CLI_EXECUTABLE --host $DBMS_IP --user $DBMS_USER"
-            fi
-        fi
-    }
-
-    main
-
-    exit 0
+        if dbms_meta[dbms_entry]["command"] == "mycli":
+            os.system('${pkgs.tmux}/bin/tmux new-window "${pkgs.mypgWorking.mycli}/bin/mycli --host {0} --user {1} --password {2}"'.format(
+                dbms_meta[dbms_entry]["ip"],
+                dbms_meta[dbms_entry]["user"],
+                dbms_pass
+            ))
+        elif dbms_meta[dbms_entry]["command"] == "mycli":
+            os.system('${pkgs.tmux}/bin/tmux new-window "PGPASSWORD={2} ${pkgs.mypgWorking.pgcli}/bin/pgcli --host {0} --user {1}"'.format(
+                dbms_meta[dbms_entry]["ip"],
+                dbms_meta[dbms_entry]["user"],
+                dbms_pass
+            ))
   '';
 in {
   options = {
