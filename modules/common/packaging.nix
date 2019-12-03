55506aaabc11f37e38a4e973cdfd93f1db6a0ed1
Alex Ermolov <aaermolov@@gmail.com>
2019-12-03 18:22:16 +0300

    modules: packaging: adapted old confctl script to niv infra

--- modules/common/packaging.nix
+++ modules/common/packaging.nix
@@ -30,6 +30,127 @@ let
     } 2>&1 > /dev/null
     ${pkgs.diffutils}/bin/diff -u --suppress-common-lines a b
   '';
+  confctl = writePythonScriptWithPythonPackages "confctl" [  # TODO: consider making relative config paths
+    pkgs.python3Packages.dmenu-python
+    pkgs.python3Packages.python-gnupg
+  ] ''
+    import glob
+    import os
+    import subprocess
+    import sys
+
+    from gnupg import GPG
+    import dmenu
+
+
+    def guess_machine_name():
+        return os.readlink("/etc/nixos/configuration.nix").split("/")[1]
+
+
+    def decrypt_secrets(machine):
+        secrets_dir = "/etc/nixos/machines/{0}/secrets".format(machine)
+        secrets = glob.glob('{0}/*.gpg'.format(secrets_dir))
+        gpg = GPG()
+        for secret in secrets:
+            secret_decrypted = os.path.splitext(secret)[0]
+            with open(secret, "rb") as src:
+                with open(secret_decrypted, "w") as dst:
+                      dst.write(str(gpg.decrypt_file(src)))
+
+    def locate_nixpkgs():
+        locate_nixpkgs_task = subprocess.Popen("nix-build /etc/nixos/nix/sources.nix -A nixpkgs --no-out-link",
+                                               shell=True, stdout=subprocess.PIPE)
+        nixpkgs_path = locate_nixpkgs_task.stdout.read().decode().strip()
+        assert locate_nixpkgs_task.wait() == 0
+        return nixpkgs_path
+
+
+    def build_configuration(path=None, debug=False):
+        build_configuration_task = subprocess.Popen("${pkgs.nix}/bin/nix build -f {0}/nixos system{1}{2}".format(
+                                                    locate_nixpkgs(),
+                                                    " --show-trace" if debug else "",
+                                                    ' -I nixos-config="{0}"'.format(path) if path else ""), shell=True,
+                                                    stdout=sys.stdout, stderr=sys.stdout)
+        result = build_configuration_task.wait()
+        if result in [1, 100]:
+            sys.exit(1)
+
+
+    def switch_configuration():
+        cwd = os.getcwd()
+        try:
+            new_system_path = os.readlink("{0}/result".format(cwd))
+        except FileNotFoundError as e:
+            new_system_path = None
+            print("Error switching configuration: {0}".format(e))
+            sys.exit(1)
+        switch_configuration_task = subprocess.Popen("pkexec ${pkgs.nix}/bin/nix-env --profile /nix/var/nix/profiles/system --set {0} && pkexec {1}/bin/switch-to-configuration switch".format(
+                                                     new_system_path, new_system_path), shell=True,
+                                                     stdout=sys.stdout, stderr=sys.stdout)
+        result = switch_configuration_task.wait()
+        if result != 0:
+            print("Error switching configuration") # TODO: add details
+            sys.exit(1)
+
+
+    def ensure_kernel_update():
+        current_kernel = os.readlink("/run/current-system/kernel")
+        booted_kernel = os.readlink("/run/booted-system/kernel")
+
+        if current_kernel != booted_kernel:
+            print("Rebooting in 5 sec...")
+            time.sleep(5)
+            os.system("reboot")
+
+
+    machine = guess_machine_name()
+
+    operations = [
+        "Update current configuration",
+        "Update current configuration (debug)",
+        "Select and build configuration",
+        "Select and build configuration (debug)",
+        "Link configuration"
+    ] # TODO: add submodules updating
+
+    operation = dmenu.show(operations, prompt='>', lines=10)
+
+    if operation == "Update current configuration":
+        decrypt_secrets(machine)
+        build_configuration()
+        switch_configuration()
+        ensure_kernel_update()
+    elif operation == "Update current configuration (debug)":
+        decrypt_secrets(machine)
+        build_configuration(debug=True)
+    elif operation == "Select and build configuration":
+        MACHINES_CONFIG_PATH = "/etc/nixos/machines"
+        config_entries = os.listdir(MACHINES_CONFIG_PATH)
+        configs = { k: v for (k, v) in zip([entry[:-4] if entry.endswith("nix") else entry for entry in config_entries],
+                    config_entries)}
+        result = dmenu.show(configs.keys(), prompt='config', lines=5)
+        if result:
+            build_configuration(path="{0}/{1}".format(MACHINES_CONFIG_PATH, configs[result]))
+    elif operation == "Select and build configuration (debug)":
+        MACHINES_CONFIG_PATH = "/etc/nixos/machines"
+        config_entries = os.listdir(MACHINES_CONFIG_PATH)
+        configs = { k: v for (k, v) in zip([entry[:-4] if entry.endswith("nix") else entry for entry in config_entries],
+                    config_entries)}
+        result = dmenu.show(configs.keys(), prompt='config', lines=5)
+        if result:
+            build_configuration(path="{0}/{1}".format(MACHINES_CONFIG_PATH, configs[result]), debug=True)
+    elif operation == "Link configuration":
+        os.chdir("/etc/nixos")
+        machines = os.listdir("machines")
+        result = dmenu.show(machines, prompt='config', lines=5)
+        if result:
+            os.remove("configuration.nix")
+            os.symlink(os.path.relpath("machines/{0}/default.nix".format(result)), "configuration.nix")
+
+
+    # TODO list
+    # implement building VM (and probably some other choices from nixos-rebuild)
+  '';
   emacsPackagingSetup = ''
     (use-package nix-mode
       :ensure t
@@ -151,6 +272,7 @@ in {
       home-manager.users."${config.attributes.mainUser.name}" = {
         home.packages = with pkgs; [
           get-pr-override
+          confctl
         ];
       };
     })
