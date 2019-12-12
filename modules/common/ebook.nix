1c04b967fdd5a6d525423032ce98928c3bb670de
Alex Ermolov <aaermolov@@gmail.com>
2019-12-12 13:36:11 +0300

    modules: ebook: enhancements
    - added epub files into account
    - correctly open files with special chars in names

--- modules/common/ebook.nix
+++ modules/common/ebook.nix
@@ -8,20 +8,21 @@ let
     pkgs.python3Packages.dmenu-python
   ] ''
     import os
+    import re
     import subprocess
 
     import dmenu
 
     books = []
 
-    books_task = subprocess.Popen("${pkgs.fd}/bin/fd --full-path /home/${config.attributes.mainUser.name}/bookshelf -e pdf -e djvu",
+    books_task = subprocess.Popen("${pkgs.fd}/bin/fd --full-path /home/${config.attributes.mainUser.name}/bookshelf -e pdf -e djvu -e epub",
                                   shell=True, stdout=subprocess.PIPE)
     books.extend([book for book in books_task.stdout.read().decode().split("\n")])
     assert books_task.wait() == 0
 
     result = dmenu.show(books, prompt='book', lines=30)
     if result:
-        os.system("${config.attributes.defaultCommands.ebookReader} {0}".format(result))
+        os.system("${config.attributes.defaultCommands.ebookReader} {0}".format(re.escape(result)))
   '';
 in {
   options = {
