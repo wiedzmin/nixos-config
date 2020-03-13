99cb2760ba6d88ed9522a991c9b1f1458f23519e
Alex Ermolov <aaermolov@gmail.com>
2020-03-13 10:15:07 +0300

    modules: packaging: emacs: explicitly set tab width to 2 in nix-mode

--- modules/common/packaging/packaging.el
+++ modules/common/packaging/packaging.el
@@ -1,6 +1,8 @@
 (use-package nix-mode
   :mode (("\\.nix$" . nix-mode)
-         ((rx (eval "configuration.nix") (zero-or-more anything) eol) . nix-mode)))
+         ((rx (eval "configuration.nix") (zero-or-more anything) eol) . nix-mode))
+  :hook
+  (nix-mode-hook . (lambda () (setq-local tab-width 2))))
 
 (use-package company-nixos-options
   :disabled
