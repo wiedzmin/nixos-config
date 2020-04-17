6bea14e8e6c26c0f7d74ddd5bcca5e60629a66f5
Alex Ermolov <aaermolov@gmail.com>
2020-04-17 22:54:59 +0300

    modules: dbms: updated script

--- modules/common/dbms/dbms.py
+++ modules/common/dbms/dbms.py
@@ -23,10 +23,20 @@ if not len(dbms_meta):
 
 dbms_entry = dmenu.show(dbms_meta.keys(), lines=5)
 if dbms_entry:
-    dbms_pass_task = subprocess.Popen("pass {0}".format(dbms_meta[dbms_entry]["passwordPassPath"]),
-                                      shell=True, stdout=subprocess.PIPE)
-    dbms_pass = dbms_pass_task.stdout.read().decode().split("\n")[0]
-    assert dbms_pass_task.wait() == 0
+    dbms_pass = None
+    if dbms_meta[dbms_entry].get("passwordPassPath"): # using pass
+        dbms_pass_task = subprocess.Popen("pass {0}".format(dbms_meta[dbms_entry]["passwordPassPath"]),
+                                          shell=True, stdout=subprocess.PIPE)
+        dbms_pass = dbms_pass_task.stdout.read().decode().split("\n")[0]
+        assert dbms_pass_task.wait() == 0
+    elif dbms_meta[dbms_entry].get("password"): # password in plaintext
+        dbms_pass = dbms_meta[dbms_entry].get("password")
+    else:
+        n = notify2.Notification("[dbms]", "No password provided for '{0}'".format(dbms_entry))
+        n.set_urgency(URGENCY_CRITICAL)
+        n.set_timeout(5000)
+        n.show()
+        sys.exit(1)
 
     dbms_vpn = dbms_meta[dbms_entry].get("vpn", None)
     if dbms_vpn:
@@ -43,7 +53,8 @@ if dbms_entry:
             dbms_pass
         ))
     elif dbms_meta[dbms_entry]["command"] == "pgcli":
-        os.system('tmux new-window "PGPASSWORD={2} @pgcliBinary@ --host {0} --user {1}"'.format(
+        # TODO: elaborate more sophisticated cmd construction logic
+        os.system('tmux new-window "PGPASSWORD={2} @pgcliBinary@ --host {0} --user {1} --no-password"'.format(
             dbms_meta[dbms_entry]["ip"],
             dbms_meta[dbms_entry]["user"],
             dbms_pass
