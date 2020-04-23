45384d26fed8b2f35ac3446c466cd55da1625e5d
Alex Ermolov <aaermolov@gmail.com>
2020-04-24 01:12:14 +0300

    modules: navigation: snippet selection should be case-insensitive

--- modules/common/navigation/insert_snippet.py
+++ modules/common/navigation/insert_snippet.py
@@ -9,6 +9,6 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 
 snippets = json.loads(r.get("misc/snippets"))
 
-snippet = dmenu.show(snippets, prompt="insert", lines=10)
+snippet = dmenu.show(snippets, prompt="insert", case_insensitive=True, lines=10)
 if snippet:
     os.system("setxkbmap && xdotool type \"{0}\"".format(snippet))
