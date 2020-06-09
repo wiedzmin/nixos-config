31a13a45ecbcf9cf1141725cdabf8a774253b207
Alex Ermolov <aaermolov@gmail.com>
2020-06-10 00:31:43 +0300

    modules: wm: i3: directly implanted most common previously used WM settings
    Generaliztion pending.

--- modules/common/wm/i3/default.nix
+++ modules/common/wm/i3/default.nix
@@ -100,24 +100,121 @@ in {
             }
             bindsym $mod+r mode "resize"
 
+            mode "run" {
+              bindsym t exec --no-startup-id ${pkgs.tmuxp_sessions}/bin/tmuxp_sessions; mode "default"
+              bindsym d exec --no-startup-id ${pkgs.dbms}/bin/dbms; workspace $ws_shell; mode "default"
+              bindsym a exec --no-startup-id ${pkgs.autorandr_profiles}/bin/autorandr_profiles; mode "default"
+              bindsym c exec --no-startup-id ${pkgs.systemd}/bin/systemctl --user restart compton.service; mode "default"
+              bindsym r exec --no-startup-id ${pkgs.reposearch}/bin/reposearch; mode "default"
+              bindsym p exec --no-startup-id ${pkgs.rofi-pass}/bin/rofi-pass; mode "default"
+              bindsym b exec --no-startup-id ${pkgs.bookshelf}/bin/bookshelf; mode "default"
+            }
+            bindsym $mod+r mode "run"
+
+            mode "network" {
+              bindsym Shift-f exec --no-startup-id ${pkgs.sshfsmenu}/bin/sshfsmenu --mode unmount; mode "default"
+              bindsym f exec --no-startup-id ${pkgs.sshfsmenu}/bin/sshfsmenu --mode mount; mode "default"
+              bindsym i exec --no-startup-id ${pkgs.ifconfless}/bin/ifconfless; mode "default"
+              bindsym d exec --no-startup-id ${pkgs.sshmenu}/bin/sshmenu --choices; workspace $ws_shell; mode "default"
+              bindsym s exec --no-startup-id ${pkgs.sshmenu}/bin/sshmenu; workspace $ws_shell; mode "default"
+              bindsym t exec --no-startup-id ${pkgs.sshmenu}/bin/sshmenu --ignore-tmux; workspace $ws_shell; mode "default"
+              bindsym u exec --no-startup-id tmux new-window ${pkgs.networkmanager}/bin/nmtui; workspace $ws_shell; mode "default"
+            }
+            bindsym $mod+n mode "network"
+
+            mode "services" {
+              bindsym s exec --no-startup-id ${pkgs.systemd}/bin/systemctl restart nscd.service; mode "default"
+              bindsym ${config.custom.networking.secrets.vpn.mnemoChar} exec --no-startup-id ${config.custom.networking.secrets.vpn.upCommand}; mode "default"
+              bindsym Shift-${config.custom.networking.secrets.vpn.mnemoChar} exec --no-startup-id ${config.custom.networking.secrets.vpn.downCommand}; mode "default"
+              bindsym ${config.job."14f7646bef".secrets.vpn.mnemoChar} exec --no-startup-id ${
+                config.job."14f7646bef".secrets.vpn.upCommand
+              }; mode "default"
+              bindsym Shift-${config.job."14f7646bef".secrets.vpn.mnemoChar} exec --no-startup-id ${
+                config.job."14f7646bef".secrets.vpn.downCommand
+              }; mode "default"
+            }
+            bindsym $mod+s mode "services"
+
+            mode "docker" {
+              bindsym t exec --no-startup-id ${pkgs.docker_containers_traits}/bin/docker_containers_traits; workspace $ws_shell; mode "default"
+              bindsym s exec --no-startup-id ${pkgs.docker_shell}/bin/docker_shell; workspace $ws_shell; mode "default"
+              bindsym i exec --no-startup-id ${pkgs.docker_swarm_services_info}/bin/docker_swarm_services_info; workspace $ws_shell; mode "default"
+            }
+            bindsym $mod+d mode "docker"
+
+            mode "windows" {
+              bindsym e exec --no-startup-id ${pkgs.emacs}/bin/emacsclient -c -a emacs; mode "default"
+              bindsym w exec --no-startup-id ${dmenu_select_windows}/bin/dmenu_select_windows; mode "default"
+              bindsym s exec --no-startup-id ${pkgs.insert_snippet}/bin/insert_snippet; mode "default"
+            }
+            bindsym $mod+w mode "windows"
+
+            bindsym XF86ScreenSaver exec --no-startup-id ${config.custom.security.lockScreenCommand}
+            bindsym $mod+p exec --no-startup-id ${pkgs.lxqt.pavucontrol-qt}/bin/pavucontrol-qt
+            bindsym $mod+Control+j exec --no-startup-id ${pkgs.srvctl}/bin/srvctl
+            bindsym $mod+Shift+u exec --no-startup-id ${pkgs.uptime_info}/bin/uptime_info
+
+            bindsym Print exec --no-startup-id ${pkgs.screenshot_active_window}/bin/screenshot_active_window
+            bindsym Control+Print exec --no-startup-id ${pkgs.screenshot_full}/bin/screenshot_full
+            bindsym $mod+Print exec --no-startup-id ${pkgs.screenshot_region}/bin/screenshot_region
+
+            bindsym XF86MonBrightnessDown exec --no-startup-id ${pkgs.light}/bin/light -U ${
+              toString config.custom.video.backlightDelta
+            }
+            bindsym XF86MonBrightnessUp exec --no-startup-id ${pkgs.light}/bin/light -A ${
+              toString config.custom.video.backlightDelta
+            }
+            bindsym XF86AudioRaiseVolume exec --no-startup-id ${pkgs.playerctl}/bin/playerctl --all-players volume ${
+              builtins.toString config.custom.sound.volume.deltaFraction
+            }+
+            bindsym XF86AudioLowerVolume exec --no-startup-id ${pkgs.playerctl}/bin/playerctl --all-players volume ${
+              builtins.toString config.custom.sound.volume.deltaFraction
+            }-
+            bindsym XF86AudioPrev exec --no-startup-id ${pkgs.playerctl}/bin/playerctl --all-players previous
+            bindsym XF86AudioPlay exec --no-startup-id ${pkgs.playerctl}/bin/playerctl --all-players play-pause
+            bindsym XF86AudioNext exec --no-startup-id ${pkgs.playerctl}/bin/playerctl --all-players next
+            bindsym XF86AudioMute exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle
+            bindsym XF86AudioMicMute exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle
+
+            bindsym Control+XF86MonBrightnessDown exec --no-startup-id ${pkgs.light}/bin/light -S 20
+            bindsym Control+XF86MonBrightnessUp exec --no-startup-id ${pkgs.light}/bin/light -S 100
+            bindsym $mod+XF86AudioNext exec --no-startup-id ${pkgs.playerctl}/bin/playerctl --all-players position ${
+              builtins.toString config.custom.content.players.deltaSeconds
+            }+
+            bindsym $mod+XF86AudioPrev exec --no-startup-id ${pkgs.playerctl}/bin/playerctl --all-players position ${
+              builtins.toString config.custom.content.players.deltaSeconds
+            }-
+
+            bindsym $mod+slash exec --no-startup-id ${pkgs.search_selection}/bin/search_selection; workspace $ws_web
+            bindsym $mod+Control+slash exec --no-startup-id ${pkgs.search_prompt}/bin/search_prompt; workspace $ws_web
+            bindsym $mod+j exec --no-startup-id ${pkgs.webjumps}/bin/webjumps; workspace $ws_web
+
+            mode "Passthrough Mode - Press M+F11 to exit" {
+              bindsym $mod+F11 mode "default"
+            }
+            bindsym $mod+F11 mode "Passthrough Mode - Press M+F11 to exit"
+
             ${mkWorkspacesI3 config.wmCommon.workspaces.primary}
             ${mkWorkspacesI3 config.wmCommon.workspaces.secondary}
             ${mkWorkspacesI3 config.wmCommon.workspaces.tertiary}
 
-            # assign [class="Emacs"] $ws1
-            # assign [class="Google-chrome"] $ws2
-            # assign [instance="Navigator"] $ws2
-            # assign [instance="main-terminal"] $ws3
-            # assign [class="Slack"] $ws4
-            # assign [class="TelegramDesktop"] $ws5
-            # assign [class="Evolution"] $ws6
+            assign [class=".*athura.*"] $ws_read
+            assign [class="^(Chromium-browser|qutebrowser)"] $ws_web
+            assign [class="^(Gimp|aft-linux-qt)"] $ws_scratch
+            assign [class="^(Skype|qutebrowser|Slack|TelegramDesktop|zoom|quassel)"] $ws_im
+            assign [class="^(Soffice|calibre)"] $ws_read
+            assign [class="^(Virt-manager|Virt-viewer|VirtualBox Manager)"] $ws_tools
+            assign [class="^Alacritty"] $ws_shell
+            assign [class="^Emacs"] $ws_work
+            assign [class="^Xsane"] $ws_scan
+            assign [class="^mpv"] $ws_media
 
-            # autostart apps
-            # exec --no-startup-id emacs
-            # exec --no-startup-id qutebrowser -P default --class qb-default
-            # exec --no-startup-id alacritty urxvt -name "main-terminal"
-            # exec --no-startup-id slack
-            # exec --no-startup-id telegram-desktop
+            exec --no-startup-id emacs
+            exec --no-startup-id qutebrowser -P default --class qb-default
+            exec --no-startup-id alacritty -name "main-terminal"
+            exec --no-startup-id telegram-desktop
+            exec --no-startup-id slack
+
             exec_always --no-startup-id ${pkgs.kbdctl}/bin/kbdctl
 
             bar {
