a94a27c26ee140a34ad3cf04b351ada8ddf049c8
Alex Ermolov <aaermolov@gmail.com>
2020-07-03 11:51:50 +0300

    machines: removed hashed passwords for root
    It makes little sense without mutableUsers = false, and also
    fires warning in recent nixpkgs

--- machines/momcat/default.nix
+++ machines/momcat/default.nix
@@ -92,8 +92,6 @@ in {
 
   environment.shells = with pkgs; [ "${bash}/bin/bash" "${zsh}/bin/zsh" ];
 
-  users.extraUsers.root.hashedPassword =
-    "586c56b7b6b6f68fca29c9ff2524e4dc52d51d5b6184a65f707dd3eae075e4c9afa81c9cd4042c26c9fb773d4f3de55fb55f363c6b0f5f6790baf4c4e3f32cb9";
   nix.trustedUsers = [ "root" config.attributes.mainUser.name ];
 
   security = {
