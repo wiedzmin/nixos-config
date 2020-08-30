7004a99a38543bd0b4c22be6c92e8ac7a3008501
Alex Ermolov <aaermolov@gmail.com>
2020-08-30 11:10:50 +0300

    nix: pystdlib: fixed `notify` parameters ordering

--- nix/public/05-pystdlib.nix
+++ nix/public/05-pystdlib.nix
@@ -7,8 +7,8 @@ self: super: {
     src = super.fetchFromGitHub {
       owner = "wiedzmin";
       repo = "pystdlib";
-      rev = "6028cf4ba0b8be0ec126003968ff39657612e7c1";
-      sha256 = "0mqv08kjpdmrp4nx104qq425crfhqal9s4qbzf9yx2qmgs2i2q89";
+      rev = "b4f95d45743196c9801fa4ffa8f4273cd7b2141e";
+      sha256 = "01lkad2dz4msn4y1qpwapdnww9gjn7f08wgab4f03ggha7sxhhql";
     };
 
     propagatedBuildInputs = with super; with python3Packages; [ dmenu-python libtmux notify2 pyfzf xlib ];
