b38f13ed39deabe843c51981ba751d9e433cb389
Alex Ermolov <aaermolov@gmail.com>
2020-10-05 16:53:50 +0300

    housekeeping: added explicit cache invalidation handle to `srvctl`

--- modules/common/housekeeping/scripts/srvctl.py
+++ modules/common/housekeeping/scripts/srvctl.py
@@ -1,8 +1,10 @@
+import argparse
 import os
 import sys
 
 import redis
 
+from pystdlib import shell_cmd
 from pystdlib.uishim import get_selection, notify
 from pystdlib.systemd import list_units, unit_perform, unit_show
 
@@ -15,15 +17,27 @@ operations = [
     "status",
 ]
 
+parser = argparse.ArgumentParser(description="SystemD services management.")
+parser.add_argument("--invalidate-cache", "-i", dest="invalidate", action="store_true",
+                    help="drop units cache from Redis")
+
+args = parser.parse_args()
 
 r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+if args.invalidate:
+    shell_cmd("pkexec systemctl daemon-reload", shell=True,
+              stdout=sys.stdout, stderr=sys.stdout)
+    r.delete("system/services")
+    sys.exit(0)
 
 services = r.lrange("system/services", 0, -1)
 if not services:
     services = list_units()
     r.lpush("system/services", *services)
 
-service = get_selection(sorted(list(dict.fromkeys([service.decode() for service in services]))), 'service', lines=20, font="@wmFontDmenu@")
+service = get_selection(sorted(list(dict.fromkeys([service for service in services]))), 'service', lines=20, font="@wmFontDmenu@")
 if not service:
     sys.exit(1)
 operation = get_selection(operations, '> ', lines=5, font="@wmFontDmenu@")
