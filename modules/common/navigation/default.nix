2831eacfdb4e15465ce4afc73b55b5a2d79788cd
Alex Ermolov <aaermolov@gmail.com>
2020-10-11 19:23:15 +0300

    navigation: removed accidental hardcode

--- modules/common/navigation/default.nix
+++ modules/common/navigation/default.nix
@@ -149,7 +149,7 @@ in {
             mainUserID = builtins.toString config.users.extraUsers."${config.attributes.mainUser.name}".uid;
             emacsServerSocketPath = "/run/user/${mainUserID}/emacs/server";
             # TODO: consider extracting to a function (for ensuring running emacs instance)
-          in "[ -f ${emacsServerSocketPath} ] && ${config.ide.emacs.package}/bin/emacsclient -s /run/user/1000/emacs/server -e '(mapcar (lambda (p) (projectile-add-known-project p)) (list ${
+          in "[ -f ${emacsServerSocketPath} ] && ${config.ide.emacs.package}/bin/emacsclient -s /run/user/${mainUserID}/emacs/server -e '(mapcar (lambda (p) (projectile-add-known-project p)) (list ${
             builtins.concatStringsSep " "
             (forEach (lib.attrValues cfg.bookmarks.entries) (entry: ''"'' + entry + ''"''))
           }))' ";
