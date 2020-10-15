54e2466ddfca996bfbe89d03dc3983ce1532d625
Alex Ermolov <aaermolov@gmail.com>
2020-10-15 23:40:42 +0300

    emacs: navigation: fixed keybindings conflict (custom prefix)

--- modules/common/emacs/emacs/base.el
+++ modules/common/emacs/emacs/base.el
@@ -446,7 +446,13 @@
   ((lisp-mode-hook emacs-lisp-mode-hook markdown-mode-hook) . smartparens-strict-mode)
   (eval-expression-minibuffer-setup-hook . smartparens-mode)
   :bind
+  ("M-s" . nil)
+  ("M-e" . sp-splice-sexp)
   (:map smartparens-mode-map
+        ("C-M-t" . sp-transpose-sexp)
+        ("M-s" . nil)
+        ("M-e" . sp-splice-sexp))
+  (:map sp-keymap
         ("C-M-t" . sp-transpose-sexp)
         ("M-s" . nil)
         ("M-e" . sp-splice-sexp))
