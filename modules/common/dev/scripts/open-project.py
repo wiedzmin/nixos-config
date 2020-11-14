b468c9578f51026d3ee130755bc38328755cd87d
Alex Ermolov <aaermolov@gmail.com>
2020-11-14 22:57:49 +0300

    dev: scripts: fixed tmux session handling in `open-project`

--- modules/common/dev/scripts/open-project.py
+++ modules/common/dev/scripts/open-project.py
@@ -5,10 +5,10 @@ import time
 
 import redis
 
-from pystdlib.uishim import get_selection
 from pystdlib import shell_cmd
+from pystdlib.shell import tmux_create_window
+from pystdlib.uishim import get_selection
 from pystdlib.xlib import switch_named_desktop
-
 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
@@ -25,7 +25,7 @@ if bookmark:
     shell = meta.get("shell", None)
     if shell:
         tmux_session = meta.get("tmux", "@tmuxDefaultSession@")
-        tmux_create_window(None, tmux_session, window_title=path.split("/")[-1], attach=True)
+        tmux_create_window(None, tmux_session, window_title=path.split("/")[-1], attach=True, start_directory=path)
     elisp_cmd = f'(dired "{path}")'
     emacs_cmd = f'emacsclient -c -s /run/user/1000/emacs/server -e \'{elisp_cmd}\' &' # TODO: make SPOT for socket path
     shell_cmd(emacs_cmd, oneshot=True)
