9f9a0b2a4e601f17a2664fe499d7f0fb25a287b5
Alex Ermolov <aaermolov@gmail.com>
2020-11-17 17:58:53 +0300

    flakes: templates: ccpp: added CMake

--- modules/common/ccpp/templates/project/shell.nix
+++ modules/common/ccpp/templates/project/shell.nix
@@ -2,7 +2,7 @@
 
 with pkgs;
 let
-  base = [ autoconf automake bear bison flex libtool pkg-config ];
+  base = [ autoconf automake bear bison flex libtool pkg-config cmake ];
   env = [ gitAndTools.pre-commit gnumake ];
   nurpkgs = pkgs.nur.repos; # refer to packages as nurpkgs.<username>.<package>
 in
