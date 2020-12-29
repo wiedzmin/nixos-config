c9a7258d01665ea9e3177ae7982cd5659104117d
Alex Ermolov <aaermolov@gmail.com>
2020-12-29 18:52:01 +0300

    security: updated screen locking command

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -32,7 +32,7 @@ in {
       lockScreenCommand = mkOption {
         description = "Command to use for screen locking";
         type = types.str;
-        default = "${pkgs.i3lock-color}/bin/i3lock-color -c 232729 && ${pkgs.xorg.xset}/bin/xset dpms force off";
+        default = "${pkgs.i3lock-color}/bin/i3lock-color --keylayout 2 -c 232729 --pass-media-keys && ${pkgs.xorg.xset}/bin/xset dpms force off";
       };
       emacs.enable = mkOption {
         type = types.bool;
