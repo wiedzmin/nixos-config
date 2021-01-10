c6b645a5c2cb8b06166696e3062a92653f12a688
Alex Ermolov <aaermolov@gmail.com>
2021-01-11 00:34:24 +0300

    dev: navigation: projects: scripts: check if emacs is running beforehand

--- modules/dev/navigation/projects/scripts/open-project.py
+++ modules/dev/navigation/projects/scripts/open-project.py
@@ -7,6 +7,7 @@ import redis
 
 from pystdlib import shell_cmd
 from pystdlib.shell import tmux_create_window
+from pystdlib.systemd import is_systemd_service_active
 from pystdlib.uishim import get_selection, notify, URGENCY_CRITICAL
 from pystdlib.xlib import switch_named_desktop
 
@@ -26,6 +27,10 @@ if bookmark:
     if shell:
         tmux_session = meta.get("tmux", "@tmuxDefaultSession@")
         tmux_create_window(None, tmux_session, window_title=path.split("/")[-1], attach=True, start_directory=path)
+    if not is_systemd_service_active("emacs", user=True):
+        notify("[bookmarks]", "Emacs service not running", urgency=URGENCY_CRITICAL, timeout=5000)
+        sys.exit(1)
+
     elisp_cmd = f'(dired "{path}")'
     emacs_cmd = f'emacsclient -c -s /run/user/1000/emacs/server -e \'{elisp_cmd}\' &' # TODO: make SPOT for socket path
     shell_cmd(emacs_cmd, oneshot=True)
--- modules/dev/navigation/projects/scripts/reposearch.py
+++ modules/dev/navigation/projects/scripts/reposearch.py
@@ -1,8 +1,9 @@
 import sys
 
-from pystdlib.uishim import get_selection, notify, URGENCY_CRITICAL
-from pystdlib.shell import tmux_create_window
 from pystdlib import shell_cmd
+from pystdlib.shell import tmux_create_window
+from pystdlib.systemd import is_systemd_service_active
+from pystdlib.uishim import get_selection, notify, URGENCY_CRITICAL, log_error
 
 
 keyword_result = get_selection([], 'keyword', font="@wmFontDmenu@")
@@ -15,6 +16,10 @@ selected_repo = get_selection(matching_repos, 'repo', case_insensitive=True, lin
 if not selected_repo:
     notify("[search repos]", "no repository selected", timeout=5000)
     sys.exit(0)
+
+if not is_systemd_service_active("emacs", user=True):
+    notify("[search repos]", "Emacs service not running", urgency=URGENCY_CRITICAL, timeout=5000)
+    sys.exit(1)
 
 elisp_cmd = f'(dired "{selected_repo}")'
 emacs_cmd = f'emacsclient -c -s /run/user/1000/emacs/server -e \'{elisp_cmd}\'' # TODO: make SPOT for socket path
