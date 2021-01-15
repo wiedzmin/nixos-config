e15ec009354d639f0f590646d261b0925d3df593
Alex Ermolov <aaermolov@gmail.com>
2021-01-15 20:28:19 +0300

    dev: golang: flakes: updated go2nix template

--- modules/dev/golang/templates/go2nix/shell.nix
+++ modules/dev/golang/templates/go2nix/shell.nix
@@ -2,15 +2,18 @@
 
 with pkgs;
 mkShell {
-  buildInputs = [ git go go2nix ];
+  buildInputs = [ git go go2nix vgo2nix];
   shellHook = ''
     export GOPATH=$(pwd)
 
     echo
     echo -e ">>>>> instructions <<<<<"
+    echo -e ">>>>> generic projects <<<<<"
     echo -e "go get <gitforge>/user/repo"
     echo -e "cd src/<gitforge>/user/repo"
     echo -e "go2nix save"
+    echo -e ">>>>> go.mode projects <<<<<"
+    echo -e "vgo2nix"
     echo
   '';
 }
