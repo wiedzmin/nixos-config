e65c121b40439d88f795e5bf0727178350399396
Alex Ermolov <aaermolov@gmail.com>
2021-01-25 22:52:12 +0300

    workstation: sound: removed extraneous pulseaudio modules

--- modules/workstation/sound/default.nix
+++ modules/workstation/sound/default.nix
@@ -45,9 +45,6 @@ in {
         systemWide = false;
         daemon.config = cfg.daemonConfig;
         extraConfig = ''
-          load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
-          load-module module-udev-detect tsched=0
-          load-module module-cork-music-on-phone
           load-module module-bluetooth-policy auto_switch=2
         '';
         extraModules = [ pkgs.pulseaudio-modules-bt ];
