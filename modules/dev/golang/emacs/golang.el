d4ea9c144da183b4fa11f4a4b1e114f3d1aa1733
Alex Ermolov <aaermolov@gmail.com>
2021-02-16 15:04:19 +0300

    dev: golang: emacs: fixed setup

--- modules/dev/golang/emacs/golang.el
+++ modules/dev/golang/emacs/golang.el
@@ -5,28 +5,15 @@
   (before-save-hook . gofmt-before-save)
   (go-mode-hook . lsp-deferred)
   (go-mode-hook . whitespace-turn-off)
-  :bind
-  (:map go-mode-map
-        ("C-k" . sp-kill-hybrid-sexp)
-        ("C-<down>" . sp-push-hybrid-sexp)
-        ("C-<right>" . sp-slurp-hybrid-sexp))
   :company (company-tabnine company-capf)
   :capf #'pcomplete-completions-at-point
   :config
-  (use-package lsp-go
-    :custom
-    (lsp-clients-go-server-args
-     '("--cache-style=always"
-       "--diagnostics-style=onsave"
-       "--format-style=goimports")))
+  (use-package lsp-go)
   ;;TODO: enable after proper setup
   (use-package dap-go
     :after lsp-mode dap-mode
     :disabled)
-  (setq gofmt-command "goimports")
-  (lsp-register-custom-settings
-   '(("gopls.completeUnimported" t t)
-     ("gopls.usePlaceholders" t t))))
+  (setq gofmt-command "goimports"))
 
 (use-package flycheck-golangci-lint
   :after (flycheck go-mode)
