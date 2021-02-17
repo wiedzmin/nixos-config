27520e5ac216068bac992e25719fd7989ca07927
Alex Ermolov <aaermolov@gmail.com>
2021-02-17 17:07:05 +0300

    dev: projectenv: added helper tools

--- modules/dev/projectenv/default.nix
+++ modules/dev/projectenv/default.nix
@@ -56,40 +56,41 @@ in {
           mkdir -p ${cfg.devEnv.backupRoot}/$dump_dir
           cp -t ${cfg.devEnv.backupRoot}/$dump_dir $devenv_filelist
         '';
-        git-restoreenv = mkShellScriptWithDeps "git-restoreenv" (with pkgs; [ gitAndTools.git coreutils git-hideenv ]) ''
-          git_root=$(git rev-parse --show-toplevel)
-          envs_list=$(ls -a ${cfg.devEnv.backupRoot} | grep $(basename $(dirname "$git_root"))_$(basename "$git_root") | sort -n -r)
-          envs_count=$(echo "$envs_list" | wc -l)
-          tip_env=$(echo "$envs_list" | head -n 1)
-          remainder_envs=$(echo "$envs_list" | tail -n +2)
+        git-restoreenv =
+          mkShellScriptWithDeps "git-restoreenv" (with pkgs; [ gitAndTools.git coreutils git-hideenv ]) ''
+            git_root=$(git rev-parse --show-toplevel)
+            envs_list=$(ls -a ${cfg.devEnv.backupRoot} | grep $(basename $(dirname "$git_root"))_$(basename "$git_root") | sort -n -r)
+            envs_count=$(echo "$envs_list" | wc -l)
+            tip_env=$(echo "$envs_list" | head -n 1)
+            remainder_envs=$(echo "$envs_list" | tail -n +2)
 
-          if [[ "$@" =~ "--tidy" ]]; then
-            for dev_env in $remainder_envs
-            do
-              rm -rf "${cfg.devEnv.backupRoot}/$dev_env"
-            done
-          fi
-
-          if [[ ! "$@" =~ "--keep-devenv" ]]; then
-            if [ "$envs_count" -gt 1 ]; then
-              rm -rf ${cfg.devEnv.backupRoot}/$tip_env
+            if [[ "$@" =~ "--tidy" ]]; then
+              for dev_env in $remainder_envs
+              do
+                rm -rf "${cfg.devEnv.backupRoot}/$dev_env"
+              done
             fi
-          fi
 
-          git-hideenv
-          git stash drop $(git stash list --max-count=1 --grep="dev-env" | cut -f1 -d":")
-          cp -a ${cfg.devEnv.backupRoot}/$tip_env/. .
+            if [[ ! "$@" =~ "--keep-devenv" ]]; then
+              if [ "$envs_count" -gt 1 ]; then
+                rm -rf ${cfg.devEnv.backupRoot}/$tip_env
+              fi
+            fi
 
-          devenv_data=$(<${cfg.devEnv.configName})
-          devenv_data_filtered=$(echo "$devenv_data" | xargs -d '\n' find 2>/dev/null)
-          devenv_filelist=$(echo "$devenv_data_filtered" | tr '\n' ' ')
-          git reset # clean up index from unrelated staged changes
-          git add -- $devenv_filelist
-        '';
+            git-hideenv
+            git stash drop $(git stash list --max-count=1 --grep="dev-env" | cut -f1 -d":")
+            cp -a ${cfg.devEnv.backupRoot}/$tip_env/. .
+
+            devenv_data=$(<${cfg.devEnv.configName})
+            devenv_data_filtered=$(echo "$devenv_data" | xargs -d '\n' find 2>/dev/null)
+            devenv_filelist=$(echo "$devenv_data_filtered" | tr '\n' ' ')
+            git reset # clean up index from unrelated staged changes
+            git add -- $devenv_filelist
+          '';
       };
 
       home-manager.users.${user} = {
-        home.packages = with pkgs; [ git-dumpenv git-hideenv git-restoreenv git-unhideenv];
+        home.packages = with pkgs; [ git-dumpenv git-hideenv git-restoreenv git-unhideenv renderizer stgit ];
 
         home.activation.ensureDevEnvBackupRoot = {
           after = [ ];
@@ -99,7 +100,9 @@ in {
       };
     })
     (mkIf (cfg.enable && config.attributes.debug.scripts) {
-      home-manager.users.${user} = { home.packages = with pkgs; [ git-dumpenv git-hideenv git-restoreenv git-unhideenv ]; };
+      home-manager.users.${user} = {
+        home.packages = with pkgs; [ git-dumpenv git-hideenv git-restoreenv git-unhideenv ];
+      };
     })
   ];
 }
