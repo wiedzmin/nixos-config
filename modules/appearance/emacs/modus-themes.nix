0f9c843b45cadf6079e732757f7b1a150a377986
Alex Ermolov <aaermolov@gmail.com>
2021-03-05 10:43:30 +0300

    appearance: emacs: added modus-themes

---
+++ modules/appearance/emacs/modus-themes.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ... }:
+with lib;
+
+let cfg = config.appearance.emacs.themes.modus;
+in {
+  options = {
+    appearance.emacs.themes.modus = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Whether to enable doom-modeline.";
+      };
+      height = mkOption {
+        type = types.int;
+        default = 25;
+        description = "Modeline height in pixels.";
+      };
+    };
+  };
+
+  config = mkMerge [
+    (mkIf (cfg.enable && config.wm.i3.enable) {
+      ide.emacs.core.extraPackages = epkgs: [ epkgs.modus-themes ];
+      ide.emacs.core.config = ''
+        (use-package modus-themes
+          :init
+          (setq modus-themes-bold-constructs t)
+          (setq modus-themes-mode-line '3d)
+          (modus-themes-load-themes)
+          (modus-themes-load-vivendi)
+          :bind
+          (:map dbFile
+                ("t" . modus-themes-toggle)))
+      '';
+    })
+  ];
+}
