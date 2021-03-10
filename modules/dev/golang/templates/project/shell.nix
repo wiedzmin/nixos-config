672fbe4d93bca643998027312d25d673e995039b
Alex Ermolov <aaermolov@gmail.com>
2021-03-10 15:05:56 +0300

    dev: golang: updated "project" template
    Automated make targets documentation.

--- modules/dev/golang/templates/project/shell.nix
+++ modules/dev/golang/templates/project/shell.nix
@@ -17,28 +17,9 @@ let
     gomp
   ];
 in mkShell {
-  buildInputs = base ++ stats ++ git ++ [
-    go-bindata
-    goconvey
-    golangci-lint
-    gomodifytags
-    gopkgs
-    gotools
-    nurpkgs.wiedzmin.gohack
-  ];
+  buildInputs = base ++ stats ++ git
+    ++ [ go-bindata goconvey golangci-lint gomodifytags gopkgs gotools nurpkgs.wiedzmin.gohack ];
   shellHook = ''
     [ -f "./.aux" ] && source ./.aux
-    echo
-    echo -e "build - build project main module"
-    echo -e "build/docker - build Docker image"
-    echo -e "generate - generate boilerplate"
-    echo -e "lint - lint sources (golangci-lint)"
-    echo -e "lint/fix - lint+fix sources (golangci-lint)"
-    echo -e "deps/download - download dependencies"
-    echo -e "deps/update - updated dependencies to recent versions"
-    echo -e "deps/gc - remove unused dependencies"
-    echo -e "publish - push current results"
-    echo -e "publish/force - push current results (is needed occasionally)"
-    echo
   '';
 }
