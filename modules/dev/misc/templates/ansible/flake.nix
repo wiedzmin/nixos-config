4168f23bbc42c48ed474ad07e0196368cf625f0f
Alex Ermolov <aaermolov@gmail.com>
2021-03-24 19:29:13 +0300

    dev: misc: templates: ansible: updated

---
+++ modules/dev/misc/templates/ansible/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Ansible environment";
+
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    nur.url = "/home/alex3rd/workspace/repos/github.com/wiedzmin/NUR";
+    unstable.url = github:NixOS/nixpkgs/{{ .inputsUnstableRev }};
+  };
+
+  outputs = { self, flake-utils, unstable, nur }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import unstable {
+          inherit system;
+          overlays = [ nur.overlay ];
+        });
+      in { devShell = import ./shell.nix { inherit pkgs; }; });
+}
