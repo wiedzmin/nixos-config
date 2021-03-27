784f606581b042865f448fdcc3d430667aedd7fa
Alex Ermolov <aaermolov@gmail.com>
2021-03-27 21:39:36 +0300

    content: misc: scripts: reworked `collect_links_on_page`

--- modules/content/misc/scripts/collect_links_on_page.py
+++ modules/content/misc/scripts/collect_links_on_page.py
@@ -1,3 +1,4 @@
+import argparse
 import re
 import sys
 from urllib.request import urlopen
@@ -5,33 +6,62 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 from pystdlib.uishim import get_selection, notify
+from pystdlib.xlib import get_active_window_traits
 from pystdlib import shell_cmd
 
 
-def is_valid_url(url):
-    return re.search("@urlRegexPy@", url) is not None
+def extract_url(str):
+    result = re.search("@urlRegexPy@", str)
+    if result:
+        return result.group()
+    return None
 
-page_url = shell_cmd("xsel -o -b")
-if page_url is not None:
-    if is_valid_url(page_url):
-        session_name = get_selection([], "save as", lines=1, font="@wmFontDmenu@")
-        if not session_name:
-            sys.exit(1)
 
-        page_content = urlopen(page_url)
-        soup = BeautifulSoup(page_content, "html.parser")
-        tags = soup.findAll("a", attrs={"href": re.compile("^https?://")})
-        org_content = [f"#+TITLE: {soup.title.string}\n", f"#+PROPERTY: url {page_url}\n"]
-        for tag in tags:
-            org_content.append(f"* {tag.get('href')}\n")
-        with open(f"@firefoxSessionsPath@/{session_name}.org", "w") as f:
-            f.writelines(org_content)
-        notify("[scrape]", f"Scraped {len(org_content) - 2} links", timeout=5000)
-        for line in org_content:
-            print(line)
+def acquire_url():
+    clipboard_contents = shell_cmd("xsel -o -b")
+    print(f"clipboard_contents: {clipboard_contents}")
+    url = extract_url(clipboard_contents)
+    print(f"url: {url}")
+    if url:
+        return url
     else:
-        print(page_url)
         notify("[scrape]", "Non-URL content in clipboard, skipping", timeout=5000)
+        win_name, win_class = get_active_window_traits()
+        print(f"win_name: {win_name} | win_class: {win_class}")
+        if win_name:
+            url = extract_url(win_name)
+            print(f"clipboard_contents: {clipboard_contents}")
+            if not url:
+                return None
+            return url
+        else:
+            return None
 
-# TODO: add fallback/primary option of checking current window title for url regexp and treat any (first)
-# match as url to process
+
+parser = argparse.ArgumentParser(description="Collect links under chosen URL.")
+parser.add_argument("--path", "-p", dest="collection_path",
+                    help="Collected links save path")
+args = parser.parse_args()
+
+
+page_url = acquire_url()
+if page_url is not None:
+    session_name = get_selection([], "save as", lines=1, font="@wmFontDmenu@")
+    if not session_name:
+        sys.exit(1)
+
+    page_content = urlopen(page_url)
+    soup = BeautifulSoup(page_content, "html.parser")
+    tags = soup.findAll("a", attrs={"href": re.compile("^https?://")})
+    org_content = [f"#+TITLE: {soup.title.string}\n", f"#+PROPERTY: url {page_url}\n"]
+    for tag in tags:
+        org_content.append(f"* {tag.get('href')}\n")
+    if args.collection_path:
+        save_path = f'{args.collection_path + "/"}{session_name}.org'
+    else:
+        save_path = f'{session_name}.org'
+    with open(save_path, "w") as f:
+        f.writelines(org_content)
+    notify("[scrape]", f"Scraped {len(org_content) - 2} links", timeout=5000)
+    for line in org_content:
+        print(line)
