8f914aaf6b284f4524cf8d7bc524203604d7cd59
Alex Ermolov <aaermolov@gmail.com>
2021-07-14 21:24:02 +0300

    appearance: colorschemes: dracula: added i3 options

--- modules/appearance/colorschemes/dracula/default.nix
+++ modules/appearance/colorschemes/dracula/default.nix
@@ -429,5 +429,27 @@ in
         statusbar-fg = "#f8f8f2";
       };
     };
+    wm.i3.theme = {
+      client = ''
+        # class                 border  bground text    indicator child_border
+        client.focused          #6272A4 #6272A4 #F8F8F2 #6272A4   #6272A4
+        client.focused_inactive #44475A #44475A #F8F8F2 #44475A   #44475A
+        client.unfocused        #282A36 #282A36 #BFBFBF #282A36   #282A36
+        client.urgent           #44475A #FF5555 #F8F8F2 #FF5555   #FF5555
+        client.placeholder      #282A36 #282A36 #F8F8F2 #282A36   #282A36
+        client.background       #F8F8F2
+      '';
+      bar = ''
+        background #282A36
+        statusline #F8F8F2
+        separator  #44475A
+
+        focused_workspace  #44475A #44475A #F8F8F2
+        active_workspace   #282A36 #44475A #F8F8F2
+        inactive_workspace #282A36 #282A36 #BFBFBF
+        urgent_workspace   #FF5555 #FF5555 #F8F8F2
+        binding_mode       #FF5555 #FF5555 #F8F8F2
+      '';
+    };
   };
 }
