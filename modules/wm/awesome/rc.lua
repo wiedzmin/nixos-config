f7b67367ff61099c83450dfb38f20d8f0a2e0fd7
Alex Ermolov <aaermolov@gmail.com>
2021-08-19 23:08:03 +0300

    wm: awesome: lua: removed extra slash in `package.path`

--- modules/wm/awesome/rc.lua
+++ modules/wm/awesome/rc.lua
@@ -1,10 +1,11 @@
 local awful = require("awful")
 local config_path = awful.util.getdir("config") -- config basedir
-package.path = config_path .. "/?.lua;" .. package.path
-package.path = config_path .. "/?/init.lua;" .. package.path
-package.path = config_path .. "/lib/?.lua;" .. package.path
-package.path = config_path .. "/lib/?/?.lua;" .. package.path
-package.path = config_path .. "/lib/?/init.lua;" .. package.path
+
+package.path = config_path .. "?.lua;" .. package.path
+package.path = config_path .. "?/init.lua;" .. package.path
+package.path = config_path .. "lib/?.lua;" .. package.path
+package.path = config_path .. "lib/?/?.lua;" .. package.path
+package.path = config_path .. "lib/?/init.lua;" .. package.path
 
 
 -- Standard awesome library
