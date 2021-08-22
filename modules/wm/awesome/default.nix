8093d8e6c3c0778f57f948ce56a2c754500c7301
Alex Ermolov <aaermolov@gmail.com>
2021-08-22 21:18:06 +0300

    wm: awesome: added logic for accessing Lua packages from Nix
    Shamelessly stolen parts of respective Nixpkgs module.

--- modules/wm/awesome/default.nix
+++ modules/wm/awesome/default.nix
@@ -6,6 +6,11 @@ with lib;
 let
   cfg = config.wm.awesome;
   user = config.attributes.mainUser.name;
+  getLuaPath = lib: dir: "${lib}/${dir}/lua/${pkgs.luaPackages.lua.luaversion}";
+  makeSearchPath = concatMapStrings (path:
+    " --search " + (getLuaPath path "share") +
+    " --search " + (getLuaPath path "lib")
+  );
 in
 {
   options = {
@@ -14,6 +19,12 @@ in
         type = types.bool;
         default = false;
         description = "Whether to enable custom config for AwesomeWM";
+      };
+      luaModules = mkOption {
+        default = [ ];
+        type = types.listOf types.package;
+        description = "List of lua packages available for being used in the Awesome configuration.";
+        example = literalExample "[ luaPackages.oocairo ]";
       };
     };
   };
@@ -25,7 +36,7 @@ in
       shell.core.variables = [{ CURRENT_WM = "awesome"; global = true; emacs = true; }];
 
       nixpkgs.config.packageOverrides = _: rec {
-        debug-awesome = mkWMDebugScript "debug-awesome" pkgs.awesome "awesome -c ~/.config/awesome/rc.lua";
+        debug-awesome = mkWMDebugScript "debug-awesome" pkgs.awesome "awesome -c ~/.config/awesome/rc.lua ${makeSearchPath cfg.luaModules}";
       };
 
       home-manager.users.${user} = {
