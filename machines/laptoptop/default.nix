9565edb718b3259ed3a9b606829fadbbc7afe946
Alex Ermolov <aaermolov@gmail.com>
2021-11-05 10:23:29 +0300

    machines: laptoptop: updated kernel version, disabled `virtualbox`

--- machines/laptoptop/default.nix
+++ machines/laptoptop/default.nix
@@ -75,7 +75,7 @@ in
     };
     initrd.availableKernelModules = [ "ahci" "ehci_pci" "sdhci_pci" "usb_storage" "xhci_pci" ];
     tmpOnTmpfs = false;
-    kernelPackages = pkgs.linuxPackages_5_4;
+    kernelPackages = pkgs.linuxPackages_5_10;
     supportedFilesystems = [ "ntfs" ];
   };
 
@@ -573,7 +573,7 @@ in
       };
     };
     libvirt.enable = true;
-    virtualbox.enable = true;
+    virtualbox.enable = false;
   };
 
   dbms = {
