5f08c5742ba0ab1a28919484ea5b343753c12ed7
Alex Ermolov <aaermolov@gmail.com>
2022-02-06 17:23:42 +0300

    machines: laptoptop: updated kernel version

--- machines/laptoptop/default.nix
+++ machines/laptoptop/default.nix
@@ -75,7 +75,7 @@ in
     };
     initrd.availableKernelModules = [ "ahci" "ehci_pci" "sdhci_pci" "usb_storage" "xhci_pci" ];
     tmpOnTmpfs = false;
-    kernelPackages = pkgs.linuxPackages_5_10;
+    kernelPackages = pkgs.linuxPackages_5_16;
     supportedFilesystems = [ "ntfs" ];
   };
 
