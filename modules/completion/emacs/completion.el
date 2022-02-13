508309b25dfe1be0ae237d93aa45ac0e9be08c8a
Alex Ermolov <aaermolov@gmail.com>
2022-02-13 21:29:08 +0300

    completion: emacs: disabled `tabnine` free version nagging

--- modules/completion/emacs/completion.el
+++ modules/completion/emacs/completion.el
@@ -125,6 +125,12 @@
   :custom
   (company-tabnine-max-num-results 10)
   :config
+  (defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
+    (let ((company-message-func (ad-get-arg 0)))
+      (when (and company-message-func
+                 (stringp (funcall company-message-func)))
+        (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
+          ad-do-it))))
   (add-to-list 'company-transformers 'company/sort-by-tabnine t)
   (add-to-list 'company-backends '(company-capf :with company-tabnine :separate)))
 
