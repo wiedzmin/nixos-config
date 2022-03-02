8fdba192625bd374ad975e3cba7a6c598d1be439
Alex Ermolov <aaermolov@gmail.com>
2022-03-03 00:07:14 +0300

    emacs: navigation: seems like `undelete-frame-mode` is not in `emacsGcc` yet
    ...but is present in `emacsPgtkGcc` for some reason. So we should actually
    check if it already there.

--- modules/emacs/navigation/navigation.el
+++ modules/emacs/navigation/navigation.el
@@ -567,7 +567,8 @@
   (unless (string-equal "i3" (getenv "CURRENT_WM"))
     (add-hook 'pre-redisplay-functions 'keep-custom-frame-title))
   (blink-cursor-mode 0)
-  (undelete-frame-mode +1)
+  (when (fboundp 'undelete-frame-mode)
+    (undelete-frame-mode +1))
   (setq-default frame-title-format custom-frame-title-format) ;; for various external tools
   (setq truncate-partial-width-windows nil))
 
