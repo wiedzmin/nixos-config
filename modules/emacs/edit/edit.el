3a51a52801d22e19e2d0179886f185dbcf881bd1
Alex Ermolov <aaermolov@gmail.com>
2022-03-02 21:50:28 +0300

    emacs: edit: also auto-revert non-file buffers

--- modules/emacs/edit/edit.el
+++ modules/emacs/edit/edit.el
@@ -155,6 +155,7 @@
   :if (string-equal "i3" (getenv "CURRENT_WM"))
   :custom
   (auto-revert-check-vc-info t)
+  (global-auto-revert-non-file-buffers t)
   :config
   (delight 'auto-revert-mode " ⟲" 'autorevert)
   (global-auto-revert-mode 1))
