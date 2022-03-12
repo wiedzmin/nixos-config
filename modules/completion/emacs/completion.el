98910ba9bdb5273dff9511ae156f239229bd1332
Alex Ermolov <aaermolov@gmail.com>
2022-03-12 19:57:43 +0300

    cmpletion: emacs: updated `tabnine` setup, bugs expected

--- modules/completion/emacs/completion.el
+++ modules/completion/emacs/completion.el
@@ -101,10 +101,12 @@
   :config
   (company-prescient-mode +1))
 
+;;; Prerequisite: Execute M-x company-tabnine-install-binary to install the TabNine binary for your system.
 (use-package company-tabnine
   :after (company)
   :preface
-  (defun company/sort-by-tabnine (candidates) ;; Integrate company-tabnine with lsp-mode
+  (defun company/sort-by-tabnine (candidates)
+    "Integrate company-tabnine with lsp-mode"
     (if (or (functionp company-backend)
             (not (and (listp company-backend) (memq 'company-tabnine company-backends))))
         candidates
@@ -120,10 +122,29 @@
             (puthash candidate t candidates-table)))
         (setq candidates-lsp (nreverse candidates-lsp))
         (setq candidates-tabnine (nreverse candidates-tabnine))
-        (nconc (seq-take candidates-tabnine 3)
-               (seq-take candidates-lsp 6)))))
+        (nconc (seq-take candidates-lsp 2)
+               (seq-take candidates-tabnine 2)
+               (seq-drop candidates-lsp 2)
+               (seq-drop candidates-tabnine 2)))))
+  (defun company-tabnine-toggle (&optional enable)
+    "Enable/Disable TabNine. If ENABLE is non-nil, definitely enable it."
+    (interactive)
+    (if (or enable (not (memq 'company-tabnine company-backends)))
+        (progn
+          (add-to-list 'company-backends #'company-tabnine)
+          (add-to-list 'company-backends '(company-capf :with company-tabnine :separate))
+          (add-to-list 'company-transformers 'company/sort-by-tabnine t)
+          (message "TabNine enabled."))
+      (setq company-backends (delete 'company-tabnine company-backends))
+      (setq company-backends (delete '(company-capf :with company-tabnine :separate) company-backends))
+      (setq company-transformers (delete 'company/sort-by-tabnine company-transformers))
+      (company-tabnine-kill-process)
+      (message "TabNine disabled.")))
+  :hook
+  (kill-emacs . company-tabnine-kill-process)
   :custom
   (company-tabnine-max-num-results 10)
+  (company-tabnine-max-restart-count 3)
   :config
   (defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
     (let ((company-message-func (ad-get-arg 0)))
@@ -132,7 +153,9 @@
         (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
           ad-do-it))))
   (add-to-list 'company-transformers 'company/sort-by-tabnine t)
-  (add-to-list 'company-backends '(company-capf :with company-tabnine :separate)))
+  ;; (add-to-list 'company-backends #'company-tabnine)
+  (add-to-list 'company-backends '(company-capf :with company-tabnine :separate))
+  (company-tabnine-toggle t))
 
 (use-package dabbrev
   :custom
