61570c8868bc6b9ea6da3f4a7ffdce26e42abde9
Alex Ermolov <aaermolov@gmail.com>
2022-08-13 00:06:05 +0300

    emacs: navigation: reverted complex `avy` setup
    As always, it takes precious mental capacity to keep
    track of the tool state itself, which could be used
    to solve actual problems. So this is really for those
    with longer/numerous "brain cache lines", or having
    less tools at hand.
    
    Nevertheless, really useful custom actions will be
    added soon.

--- modules/emacs/navigation/navigation.el
+++ modules/emacs/navigation/navigation.el
@@ -8,9 +8,6 @@
 
 (use-package avy
   :preface
-  (defvar my-avy-point nil)
-  (defvar processing-compound? nil)
-  (defvar avy-interrupted? nil)
   (defun avy-generic-command-action (action-f)
     "Excecutes action-f at point and stays"
     (save-excursion
@@ -19,58 +16,6 @@
     (select-window
      (cdr (ring-ref avy-ring 0)))
     t)
-  (defun avy-action-mark-point (pt)
-    "Sets a point for other commands"
-    (setq my-avy-point pt)
-    (select-window
-     (cdr
-      (ring-ref avy-ring 0)))
-    (message "Point set!"))
-  (defun avy--quick-mark-region (pt)
-    "Intermediate function to mark regions, used in region actions"
-    (when (> my-avy-point pt)
-      (progn
-        (setf aux pt)
-        (setf pt my-avy-point)
-        (setf my-avy-point aux)))
-    (goto-char my-avy-point)
-    (set-mark my-avy-point)
-    (activate-mark)
-    (goto-char (+ 1 pt))
-    (setq my-avy-point nil))
-  (defun avy--return-point-region-action ()
-    "Makes sure that the point returns to its original place even if it is in another window"
-    (let ((dat (ring-ref avy-ring 0)))
-      (select-frame-set-input-focus
-       (window-frame (cdr dat)))
-      (select-window (cdr dat))
-      (goto-char (car dat))))
-  (defun avy--check-for-region-errors ()
-    "Checks if set point action was previously made, cleans action otherwise"
-    (progn (message "No point set")
-           (avy--return-point-region-action)
-           nil))
-  (defun avy-set-auto-action (res action)
-    "Sets an auto action to excecute on the next avy interaction, it is necessary to unset afterwards if not longer desired
-res param is needed for default behavior"
-    (avy-pre-action-default res)
-    (setq avy-action action))
-  (defun avy-unset-auto-action ()
-    "Unsets an auto action"
-    (setq avy-pre-action 'avy-pre-action-default))
-  (defun clean-avy-auto-action ()
-    "Resets default  avy preaction and compound flag"
-    (avy-unset-auto-action)
-    (setq processing-compound? nil))
-  (defun avy-intial-interruption-check (&optional arg)
-    "Function to run before avy command to check if the last action was interrupted"
-    (if avy-interrupted?
-        (progn
-          (clean-avy-auto-action))
-      (setq avy-interrupted? t)))
-  (defun avy-set-no-interruption (&optional arg)
-    "Function to run after avy command to state that there were no interruption"
-    (setq avy-interrupted? nil))
   (defun avy-action-mark-to-char (pt)
     (activate-mark)
     (goto-char (+ 1 pt)))
@@ -102,30 +47,6 @@ res param is needed for default behavior"
     "Exchange sexp at PT with the one at point."
     (set-mark pt)
     (transpose-sexps 0))
-  (defun avy-auto-set-point-preaction (res)
-    "Preaction for setting the point as part of a region action"
-    (avy-set-auto-action res 'avy-action-mark-point))
-  (defun avy-auto-yank-preaction (res)
-    "Preaction for yanking"
-    (avy-set-auto-action res 'avy-action-yank))
-  (defun avy-replace-action (pt)
-    "Compound action for replacing a candidate with another candidate"
-    (avy-action-kill-move pt)
-    (setq avy-pre-action 'avy-auto-yank-preaction)
-    (setq avy-interrupted? nil) ; next goto-char cleans
-    (call-interactively 'avy-goto-char-timer)
-    (clean-avy-auto-action)
-    (call-interactively 'avy-pop-mark)
-    (call-interactively 'avy-pop-mark))
-  (defun avy-region-compound ()
-    "Compound for manipulating regions"
-    (interactive)
-    (setq processing-compound? t)
-    (setq avy-pre-action 'avy-auto-set-point-preaction)
-    (when (call-interactively 'avy-goto-char-timer) ; nil if interrupted
-      (avy-unset-auto-action)
-      (call-interactively 'avy-goto-char-timer))
-    (clean-avy-auto-action))
   (defun avy-action-clone-line (pt)
     (goto-char pt)
     (move-beginning-of-line 1)
@@ -135,67 +56,8 @@ res param is needed for default behavior"
     (yank)
     (indent-for-tab-command)
     t)
-  (defun avy-action-copy-region (pt)
-    "Copy region and stays"
-    (if my-avy-point
-        (progn
-          (save-excursion
-            (avy--quick-mark-region pt)
-            (call-interactively 'kill-ring-save))
-          (avy--return-point-region-action)
-          (message "Copied: %s" (current-kill 0))
-          t)
-      (avy--check-for-region-errors)))
-  (defun avy-action-yank-region (pt)
-    "Yank region and stays"
-    (avy-action-copy-region pt)
-    (yank)
-    t)
-  (defun avy-action-kill-region-move (pt)
-    "Kills a region and moves"
-    (if my-avy-point
-        (progn
-          (avy--quick-mark-region pt)
-          (call-interactively 'kill-region)
-          (message "Killed: %s" (current-kill 0))
-          (point)
-          t)
-      (avy--check-for-region-errors)))
-  (defun avy-action-teleport-region (pt)
-    "Teleports an arbitrary region using my-avy-point"
-    (if my-avy-point
-        (progn
-          (save-excursion
-            (avy--quick-mark-region pt)
-            (call-interactively 'kill-region))
-          (select-window
-           (cdr
-            (ring-ref avy-ring 0)))
-          (yank)
-          t)
-      (avy--check-for-region-errors)))
-  (defun avy-action-yank-proxy (pt)
-    "Applies regular or region based yank"
-    (if processing-compound?
-        (avy-action-yank-region pt)
-      (avy-action-yank pt)))
-  (defun avy-action-kill-proxy (pt)
-    "Applies regular or region based kill"
-    (if processing-compound?
-        (avy-action-kill-region-move pt)
-      (avy-action-kill-move pt)))
-  (defun avy-action-teleport-proxy (pt)
-    "Applies regular or region based teleport"
-    (if processing-compound?
-        (avy-action-teleport-region pt)
-      (avy-action-teleport pt)))
-  (defun avy-action-copy-proxy (pt)
-    "Applies regular or region based copy"
-    (if processing-compound?
-        (avy-action-copy-region pt)
-      (avy-action-copy pt)))
   :bind
-  ("C-:" . avy-region-compound)
+  ("C-:" . avy-goto-char)
   (:map custom-goto-map
         ("M-d" . avy-goto-word-0)
         ("M-s" . avy-goto-char-timer)
@@ -209,31 +71,19 @@ res param is needed for default behavior"
   (setq avy-style 'at-full)
   :custom-face (avy-goto-char-timer-face ((nil (:foreground "green" :weight bold))))
   :config
-  (advice-add 'avy-goto-char-timer :before #'avy-intial-interruption-check)
-  (advice-add 'avy-goto-char-timer :after #'avy-set-no-interruption)
   ;NOTE: removed 'avy-dispatch-alist vs 'avy-keys conflicts
   (setf avy-dispatch-alist (assq-delete-all ?z avy-dispatch-alist)
         avy-dispatch-alist (assq-delete-all ?x avy-dispatch-alist)
-        avy-dispatch-alist (assq-delete-all ?c avy-dispatch-alist)
-        avy-dispatch-alist (assq-delete-all ?n avy-dispatch-alist))
+        avy-dispatch-alist (assq-delete-all ?c avy-dispatch-alist))
   (setf (alist-get ?Z avy-dispatch-alist) 'avy-action-zap-to-char
         (alist-get ?, avy-dispatch-alist) 'avy-action-embark
         (alist-get ?. avy-dispatch-alist) 'avy-action-mark-point
-        (alist-get ?\C-y avy-dispatch-alist) 'avy-action-yank-proxy
-        (alist-get ?\M-w avy-dispatch-alist) 'avy-action-copy-proxy
         (alist-get ?W avy-dispatch-alist) 'avy-action-copy-whole-line
         (alist-get ?Y avy-dispatch-alist) 'avy-action-yank-whole-line
         (alist-get ?E avy-dispatch-alist) 'avy-action-exchange
-        (alist-get ?\C-t avy-dispatch-alist) 'avy-action-teleport-proxy
         (alist-get ?\C-h avy-dispatch-alist) 'avy-action-helpful
-        (alist-get ?\C-r avy-dispatch-alist) 'avy-replace-action
         (alist-get ?\C-  avy-dispatch-alist) 'avy-action-mark-to-char
-        (alist-get ?\C-l avy-dispatch-alist) 'avy-action-clone-line
-        (alist-get ?\M-W avy-dispatch-alist) 'avy-action-copy-region
-        (alist-get ?\C-Y avy-dispatch-alist) 'avy-action-yank-region
-        (alist-get ?\C-K avy-dispatch-alist) 'avy-action-kill-region-move
-        (alist-get ?\C-T avy-dispatch-alist) 'avy-action-teleport-region
-        (alist-get ?\C-k avy-dispatch-alist) 'avy-action-kill-proxy)
+        (alist-get ?\C-l avy-dispatch-alist) 'avy-action-clone-line)
   (avy-setup-default))
 
 (use-package goto-addr
@@ -351,6 +201,8 @@ res param is needed for default behavior"
              ;; Pass relative file names for dired.
              minibuffer-completing-file-name))))
   :bind
+  ;;TODO: embark-act-all
+  ;;TODO: embark-bindings
   ("C-S-a" . embark-act)
   ("C-S-d" . embark-dwim)
   (:map embark-general-map
