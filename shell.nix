21f313567948e88cf155175a8573766fa3f4ae8d
Alex Ermolov <aaermolov@gmail.com>
2022-08-29 00:16:32 +0300

    flakes: shell: `vim` is intermittently needed

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
 with pkgs;
-mkShell { buildInputs = [ cloc deadnix gitAndTools.git-crypt gitAndTools.pre-commit just nixUnstable nixfmt nixpkgs-fmt shfmt ]; }
+mkShell { buildInputs = [ cloc deadnix gitAndTools.git-crypt gitAndTools.pre-commit just nixUnstable nixfmt nixpkgs-fmt shfmt vim ]; }
