bde344f06048cc5641d09b33c0627e53eb7fdc4b
Alex Ermolov <aaermolov@gmail.com>
2022-12-06 10:16:35 +0300

    machines: laptoptop: seasonal wallpaper switch

--- machines/laptoptop/default.nix
+++ machines/laptoptop/default.nix
@@ -161,8 +161,8 @@ in
     wallpaper = {
       enable = true;
       boot.splashImage = "${inputs.nixos-artwork}/wallpapers/nix-wallpaper-mosaic-blue.png";
-      rootDir = homePrefix user "blobs/wallpaper/mongol/autumn";
-      current = "cliffs_horses-1920.jpg";
+      rootDir = homePrefix user "blobs/wallpaper/mongol/winter";
+      current = "mongol-winter-govi.jpg";
       wm.enable = true;
     };
     xresources = {
