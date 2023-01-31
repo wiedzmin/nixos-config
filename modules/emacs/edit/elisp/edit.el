b0c56e12f2f57e026c4fdda037d17fc7fc10aadf
Alex Ermolov <aaermolov@gmail.com>
2023-01-31 11:53:27 +0300

    emacs: edit: updated `expand-region` setup

--- modules/emacs/edit/elisp/edit.el
+++ modules/emacs/edit/elisp/edit.el
@@ -1,6 +1,9 @@
 (use-package expand-region
   :bind
-  ("C-," . er/expand-region))
+  ("C-," . er/expand-region)
+  :config
+  (add-to-list 'expand-region-exclude-text-mode-expansions 'org-mode)
+  (add-to-list 'expand-region-exclude-text-mode-expansions 'LaTeX-mode))
 
 (use-package highlight-numbers
   :hook
