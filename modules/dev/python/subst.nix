f4ae0f06920d6b9c40ff043bd866f8da17c5b782
Alex Ermolov <aaermolov@gmail.com>
2023-03-03 14:52:23 +0300

    dev: python: python-language-server --> python-lsp-server
    `python-language-server` is unmaintained and was removed from Nixpkgs.

--- modules/dev/python/subst.nix
+++ modules/dev/python/subst.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 
 rec {
-  lspPythonMsExecutable = "${pkgs.python-language-server}/bin/python-language-server";
+  lspPythonMsExecutable = "${pkgs.python3Packages.python-lsp-server}/bin/python-lsp-server";
   lspPythonMsExtraPaths = builtins.concatStringsSep " " (lib.forEach config.dev.python.pylsExtraSourcePaths (path: ''"${path}"''));
 }
