74e1b0bafb39fe848df28658d45b4319812f786e
Alex Ermolov <aaermolov@gmail.com>
2023-03-04 17:02:33 +0300

    emacs: edit: removed forgotten code duplication

--- modules/emacs/edit/elisp/edit.el
+++ modules/emacs/edit/elisp/edit.el
@@ -177,15 +177,6 @@
   :custom
   (setq kmacro-ring-max 16))
 
-(use-package autorevert
-  :if (string-equal "i3" (getenv "CURRENT_WM"))
-  :custom
-  (auto-revert-check-vc-info t)
-  (global-auto-revert-non-file-buffers t)
-  :config
-  (delight 'auto-revert-mode " ⟲" 'autorevert)
-  (global-auto-revert-mode 1))
-
 (use-package misc
   :bind
   (:map mode-specific-map
