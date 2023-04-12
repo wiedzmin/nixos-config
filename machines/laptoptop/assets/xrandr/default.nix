3bc31a05678c9fa4816a8487a07d4e14212e412e
Alex Ermolov <aaermolov@gmail.com>
2023-04-12 16:53:02 +0300

    laptoptop: assets: added new autorandr profile

--- machines/laptoptop/assets/xrandr/default.nix
+++ machines/laptoptop/assets/xrandr/default.nix
@@ -1,1 +1,1 @@
-{ ... }: { imports = [ ./docked_home.nix ./docked_home_dualhead.nix ./docked_home_dualhead_swapped.nix ./mobile.nix ]; }
+{ ... }: { imports = [ ./docked_home.nix ./docked_home_dualhead.nix ./docked_home_dualhead_swapped.nix ./docked_home_dualhead_main_rotated.nix ./mobile.nix ]; }
