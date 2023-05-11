e485054dc632b51dae19aab08ecd2d2f57e5d51a
Alex Ermolov <aaermolov@gmail.com>
2023-05-11 18:17:48 +0300

    laptoptop: assets: xrandr: added layouts indirection level
    This fixes situation, when there are multiple xrandr layouts mapped under single fingerprint.
    With added indirection we can choose desired heads layout, leaving fingerprint and configuration
    remainder intact, and removing fingerprints ambiguity.

--- machines/laptoptop/assets/xrandr/docked_home_dualhead.nix
+++ machines/laptoptop/assets/xrandr/docked_home_dualhead.nix
@@ -31,34 +31,7 @@ in
             "${config.attributes.hardware.monitors.internalHead.name}" =
               config.attributes.hardware.monitors.internalHead.edid;
           };
-          config = {
-            "${config.attributes.hardware.monitors.externalPrimaryHead.name}" = {
-              enable = true;
-              position = "0x0";
-              mode = "1920x1080";
-              gamma = config.workstation.randr.defaults.gamma;
-              rate = config.workstation.randr.defaults.rate;
-            } // lib.optionalAttrs (config.workstation.randr.heads.orientation.primary != "normal") {
-              rotate = config.workstation.randr.heads.orientation.primary;
-            };
-            "${config.attributes.hardware.monitors.externalSecondaryHead.name}" = {
-              enable = true;
-              position = "1366x1080";
-              mode = "1920x1200";
-              gamma = config.workstation.randr.defaults.gamma;
-              rate = config.workstation.randr.defaults.rate;
-            } // lib.optionalAttrs (config.workstation.randr.heads.orientation.secondary != "normal") {
-              rotate = config.workstation.randr.heads.orientation.secondary;
-            };
-            "${config.attributes.hardware.monitors.internalHead.name}" = {
-              enable = true;
-              primary = true;
-              position = "0x1080";
-              mode = config.attributes.hardware.monitors.internalHead.resolution;
-              gamma = config.workstation.randr.defaults.gamma;
-              rate = config.workstation.randr.defaults.rate;
-            };
-          };
+          config = config.attributes.hardware.monitors.layouts.twoExtHeadsInternalDownright;
           hooks.postswitch = lib.optionalString (config.wm.i3.enable) "rescreen-${profileName}-i3";
         };
       };
---
+++ machines/laptoptop/assets/xrandr/layouts/layout_two_extheads_internal_atright.nix
@@ -0,0 +1,34 @@
+{ config, lib, ... }:
+
+{
+  config.attributes.hardware.monitors.layouts = {
+    twoExtHeadsInternalAtRight = {
+      "${config.attributes.hardware.monitors.externalPrimaryHead.name}" = {
+        enable = true;
+        position = "0x0";
+        mode = "1920x1080";
+        gamma = config.workstation.randr.defaults.gamma;
+        rate = config.workstation.randr.defaults.rate;
+      } // lib.optionalAttrs (config.workstation.randr.heads.orientation.primary != "normal") {
+        rotate = config.workstation.randr.heads.orientation.primary;
+      };
+      "${config.attributes.hardware.monitors.externalSecondaryHead.name}" = {
+        enable = true;
+        position = "1366x1080";
+        mode = "1920x1200";
+        gamma = config.workstation.randr.defaults.gamma;
+        rate = config.workstation.randr.defaults.rate;
+      } // lib.optionalAttrs (config.workstation.randr.heads.orientation.secondary != "normal") {
+        rotate = config.workstation.randr.heads.orientation.secondary;
+      };
+      "${config.attributes.hardware.monitors.internalHead.name}" = {
+        enable = true;
+        primary = true;
+        position = "0x1080";
+        mode = config.attributes.hardware.monitors.internalHead.resolution;
+        gamma = config.workstation.randr.defaults.gamma;
+        rate = config.workstation.randr.defaults.rate;
+      };
+    };
+  };
+}
