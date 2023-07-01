c8da08dd7e87883f31a6bcfca186a403f36e7fb9
Alex Ermolov <aaermolov@gmail.com>
2023-07-01 22:03:25 +0300

    zsh: actualized renamed `home-manager` option name

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -57,7 +57,7 @@ in
           enable = true;
           enableAutosuggestions = true;
           enableCompletion = true;
-          enableSyntaxHighlighting = true;
+          syntaxHighlighting.enable = true;
           history = {
             size = 10000;
             save = 10000;
