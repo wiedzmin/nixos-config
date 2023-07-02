3145a8b5f7524357ea9cb2a872d9a64016d38d8c
Alex Ermolov <aaermolov@gmail.com>
2023-07-03 00:17:57 +0300

    nix: core: parameterized LSP server implementation usage

--- modules/nix/core/subst.nix
+++ modules/nix/core/subst.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   nixLspInitSection =
@@ -8,4 +8,11 @@
   nixLspHookSection =
     if config.ext.nix.core.lsp.enable then ''
       (nix-mode-hook . lsp-deferred)'' else "";
+  nixLspNixdSection =
+    if config.ext.nix.core.lsp.server == "nixd" then ''
+      (lsp-register-client
+       (make-lsp-client :new-connection (lsp-stdio-connection "${pkgs.nixd}/bin/nixd")
+                        :major-modes '(nix-mode)
+                        :server-id 'nixd))
+    '' else "";
 }
