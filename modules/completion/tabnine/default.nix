595bd2635ff191349ded3252c1dfb884df6189ed
Alex Ermolov <aaermolov@gmail.com>
2023-08-03 20:15:43 +0300

    completion: tabnine: parameterized emacs client implementation

--- modules/completion/tabnine/default.nix
+++ modules/completion/tabnine/default.nix
@@ -21,6 +21,11 @@ in
         default = false;
         description = "Whether to enable Emacs TabNine setup";
       };
+      emacs.package = mkOption {
+        type = types.enum [ "company-tabnine" "tabnine" ];
+        default = "company-tabnine";
+        description = "Which Emacs package to use for speaking to TabNine";
+      };
       useNixpkgsVersion = mkOption {
         type = types.bool;
         default = false; # NOTE: nixpkgs version is currently broken (invalid interpreter)
@@ -58,10 +63,14 @@ in
       };
     })
     (mkIf (cfg.enable && cfg.emacs.enable) {
-      ide.emacs.core.extraPackages = epkgs: optionals (config.ide.emacs.completion.backend == "company") [
+      ide.emacs.core.extraPackages = epkgs: optionals (cfg.emacs.package == "company-tabnine") [
         epkgs.company-tabnine
+      ] ++ optionals (cfg.emacs.package == "tabnine") [
+        epkgs.tabnine
       ];
-      ide.emacs.core.config = readSubstituted config inputs pkgs [ ./subst.nix ] [ ./elisp/tabnine.el ];
+      ide.emacs.core.config =
+        optionalString (cfg.emacs.package == "company-tabnine") (readSubstituted config inputs pkgs [ ./subst.nix ] [ ./elisp/company-tabnine.el ]) +
+        optionalString (cfg.emacs.package == "tabnine") (readSubstituted config inputs pkgs [ ./subst.nix ] [ ./elisp/tabnine.el ]);
     })
   ];
 }
