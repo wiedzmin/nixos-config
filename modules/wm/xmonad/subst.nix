cbdca116b7f5165bcea50982a4da00bd5a909f9d
Alex Ermolov <aaermolov@gmail.com>
2023-08-23 15:58:32 +0300

    wm: xmonad: use haskell config with substitutions instead of text patch

--- modules/wm/xmonad/subst.nix
+++ modules/wm/xmonad/subst.nix
@@ -1,7 +1,18 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 with pkgs.unstable.commonutils;
 
 {
+  dwmLayoutFont = lib.optionalString (config.wmCommon.fonts.default != "") ''fontName = "${config.wmCommon.fonts.default}"'';
+
+  htopBinary = "${pkgs.htop}/bin/htop";
+  iotopBinary = "${pkgs.iotop}/bin/iotop";
+  gotopBinary = "${pkgs.gotop}/bin/gotop";
+  bcBinary = "${pkgs.bc}/bin/bc";
+  xmobarBinary = "${pkgs.xmobar}/bin/xmobar";
+
+  keysXmonadRaw = mkKeysXmonadRaw config.wm.xmonad.internalKeys 18;
+  keysXmonadSpawn = mkKeysXmonadSpawn config.wmCommon.keybindings.entries 18;
+
   xmobarMaybeFont = ""; # FIXME: reveal correct value (was lost)
   xmonadPrimaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces.primary 20;
   xmonadSecondaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces.secondary 22;
