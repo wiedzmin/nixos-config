a769fb92c5f8e0f562868269e5e149980bb2070d
Alex Ermolov <aaermolov@gmail.com>
2023-08-23 16:31:44 +0300

    wm: xmonad: fixed compilation failure for workspaces generation code

--- modules/wm/xmonad/subst.nix
+++ modules/wm/xmonad/subst.nix
@@ -14,7 +14,7 @@ with pkgs.unstable.commonutils;
   keysXmonadSpawn = mkKeysXmonadSpawn config.wmCommon.keybindings.entries 18;
 
   xmobarMaybeFont = ""; # FIXME: reveal correct value (was lost)
-  xmonadPrimaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces.primary 20;
-  xmonadSecondaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces.secondary 22;
-  xmonadTertiaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces.tertiary 20;
+  xmonadPrimaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces "primary" 20;
+  xmonadSecondaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces "secondary" 22;
+  xmonadTertiaryWorkspaces = mkWorkspacesXmonad config.wmCommon.workspaces "tertiary" 20;
 }
