23d1f83a5e946064b9101983540b03eca25f4a57
Alex Ermolov <aaermolov@gmail.com>
2023-09-17 19:36:58 +0300

    dev: ccpp: added configuration for C++ major modes

--- modules/dev/ccpp/elisp/standard.el
+++ modules/dev/ccpp/elisp/standard.el
@@ -6,6 +6,13 @@
   ;; (use-package dap-cpptools)
   (setq-default c-basic-offset 2))
 
+(use-package c++-mode
+  :mode (("\\.cpp$" . c++-mode)
+         ("\\.h$" . c++-mode)
+         ("\\.hpp$" . c++-mode))
+  :config
+  (setq-default c-basic-offset 2))
+
 (use-package ccls
   :preface
   (when (featurep 'lsp-ui)
