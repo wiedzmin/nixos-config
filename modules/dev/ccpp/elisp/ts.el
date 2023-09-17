f1ab9d2776de04b4e74c2cc350c5519c34550baa
Alex Ermolov <aaermolov@gmail.com>
2023-09-17 19:36:58 +0300

    dev: ccpp: added configuration for C++ major modes

--- modules/dev/ccpp/elisp/ts.el
+++ modules/dev/ccpp/elisp/ts.el
@@ -7,3 +7,10 @@
   ((c-ts-mode-hook c++-ts-mode-hook) . lsp-deferred)
   :config
   (setq-default c-basic-offset 2))
+
+(use-package c++-ts-mode
+  :mode (("\\.cpp$" . c++-ts-mode)
+         ("\\.h$" . c++-ts-mode)
+         ("\\.hpp$" . c++-ts-mode))
+  :config
+  (setq-default c-basic-offset 2))
