dbee402669dbdc7c1e217d97ded2528bb9061800
Alex Ermolov <aaermolov@gmail.com>
2023-11-17 23:13:08 +0300

    appearance: emacs: added window dividers setup

--- modules/appearance/emacs/default.nix
+++ modules/appearance/emacs/default.nix
@@ -13,12 +13,22 @@ in
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = "Whether to enable Emacs appearance customization.";
+        description = "Whether to enable Emacs appearance customization";
       };
       currentLineHighlightFace = mkOption {
         description = "Face for highlighting current line in buffer";
         type = types.str;
         default = "";
+      };
+      windowDivider.enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Whether to enable windows internal divider";
+      };
+      windowDivider.width = mkOption {
+        description = "Windows internal divider width";
+        type = types.int;
+        default = 4;
       };
     };
   };
