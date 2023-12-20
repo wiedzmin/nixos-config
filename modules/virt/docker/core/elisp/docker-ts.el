2555e694ba6ac6cf4a30d10221d399b8a135886f
Alex Ermolov <aaermolov@gmail.com>
2023-12-20 11:55:45 +0300

    virt: docker: core: actualized setup
    - extracted common part
    - separated configuration for treesitter-powered major mode

---
+++ modules/virt/docker/core/elisp/docker-ts.el
@@ -0,0 +1,9 @@
+(use-package dockerfile-ts-mode
+  :mode ("Dockerfile" . dockerfile-ts-mode)
+  :hook
+  (dockerfile-ts-mode-hook . lsp-deferred)
+  :config
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("docker-langserver" "--stdio"))
+                    :activation-fn (lsp-activate-on "dockerfile")
+                    :server-id 'dockerlsp)))
