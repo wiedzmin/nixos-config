114f2047dc28d36c1a326101e2b1d135a67ba9cc
Alex Ermolov <aaermolov@gmail.com>
2024-07-06 23:56:38 +0300

    appearance: fonts: laptoptop: added Martian Mono

--- machines/laptoptop/default.nix
+++ machines/laptoptop/default.nix
@@ -158,7 +158,7 @@ in
     fonts = {
       enable = true;
       antialias = true; # NOTE: use `nix-index consolefonts` to search values for `console.font`
-      iosevka.enable = true;
+      martian.enable = true;
     };
     wallpaper = {
       enable = true;
