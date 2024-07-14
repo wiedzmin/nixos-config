e753ebdb6a71bb2bf5e7ec96240832c9c9b0fcef
Alex Ermolov <aaermolov@gmail.com>
2024-07-15 00:33:44 +0300

    emacs: completion: do not wire `tempel` into completions
    It works confusing this way, and better be used explicitly.

--- modules/emacs/completion/elisp/tempel.el
+++ modules/emacs/completion/elisp/tempel.el
@@ -1,16 +1,4 @@
 (use-package tempel
-  :preface
-  (defun tempel-setup-capf ()
-    ;; Add the Tempel Capf to `completion-at-point-functions'.
-    ;; `tempel-expand' only triggers on exact matches. Alternatively use
-    ;; `tempel-complete' if you want to see all matches, but then you
-    ;; should also configure `tempel-trigger-prefix', such that Tempel
-    ;; does not trigger too often when you don't expect it. NOTE: We add
-    ;; `tempel-expand' *before* the main programming mode Capf, such
-    ;; that it will be tried first.
-    (setq-local completion-at-point-functions
-                (cons #'tempel-expand
-                      completion-at-point-functions)))
   :bind
   (:map misc-editing-map
         ("f" . tempel-complete)
@@ -18,10 +6,6 @@
   (:map tempel-map
         ("M-<right>" . tempel-next)
         ("M-<left>" . tempel-previous))
-  :hook
-  (prog-mode-hook . tempel-setup-capf)
-  (text-mode-hook . tempel-setup-capf)
-  (prog-mode-hook . tempel-abbrev-mode)
   :custom
   (tempel-path "@emacsTempelSnippetsPath@")
   ;; (tempel-trigger-prefix "<")
