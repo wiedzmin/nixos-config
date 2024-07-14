773f4591b6084dd7b483a431424e11cb0cdada87
Alex Ermolov <aaermolov@gmail.com>
2024-07-15 01:31:44 +0300

    emacs: completion: updated `dabbrev` configuration

--- modules/emacs/completion/elisp/completion.el
+++ modules/emacs/completion/elisp/completion.el
@@ -1,8 +1,17 @@
 (use-package dabbrev
+  ;; Swap M-/ and C-M-/
+  :bind (("M-/" . dabbrev-completion)
+         ("C-M-/" . dabbrev-expand))
   :custom
   (dabbrev-case-distinction nil)
   (dabbrev-case-fold-search t)
-  (dabbrev-case-replace nil))
+  (dabbrev-case-replace nil)
+  :config
+  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
+  ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
+  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
 
 (use-package all-the-icons-completion
   :after (marginalia all-the-icons)
