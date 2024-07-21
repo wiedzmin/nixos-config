490c06861c27d17f09e4096d63aea4e3ee0b50d0
Alex Ermolov <aaermolov@gmail.com>
2024-07-21 13:07:51 +0300

    appearance: fonts: selectively added padding for `i3status-rs`

--- modules/appearance/fonts/agave-fonts.nix
+++ modules/appearance/fonts/agave-fonts.nix
@@ -46,6 +46,8 @@ in
     wmCommon.fonts.familySize.large = makeFamilySizeStr (fontBeautified // { size = sizeFamilySizeLarge; });
     wmCommon.fonts.deaddFamily = fontBeautified.family;
 
+    wm.i3.statusbar.i3-rs.iconPadding = " ";
+
     wm.xmonad.xmobar.font = makeFontStrXft baseFont;
 
     shell.core.variables = [{ TB_SELECTOR_FONT = makeFontStrColons baseFont; global = true; }];
