b1b70f701acb7647a482907a4223a34cac661400
Alex Ermolov <aaermolov@gmail.com>
2024-07-28 19:07:54 +0300

    emacs: core: added option for system-wide Emacs keybindings
    ...using keyboard remapper tool.

--- modules/emacs/core/default.nix
+++ modules/emacs/core/default.nix
@@ -288,6 +288,11 @@ in
         default = false;
         description = "Whether to enable system-wide popup Emacs windows for quick edits (package `emacs-everywhere`)";
       };
+      remapEverywhere.enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Whether to use keyboard remapper to provide main Emacs-familiar keybindings in other applications";
+      };
     };
   };
 
@@ -405,6 +410,81 @@ in
             };
           }
         );
+    })
+    (mkIf (cfg.enable && cfg.remapEverywhere.enable) {
+      # TODO: consider adding settings for XKeysnail
+      workstation.input.keyboard.xremap.config = {
+        keymap = [
+          {
+            name = "Emacs";
+            application = { not = "Emacs"; };
+            remap = {
+              # Cursor
+              "C-b" = { with_mark = "left"; };
+              "C-f" = { with_mark = "right"; };
+              "C-p" = { with_mark = "up"; };
+              "C-n" = { with_mark = "down"; };
+              # Forward/Backward word
+              "M-b" = { with_mark = "C-left"; };
+              "M-f" = { with_mark = "C-right"; };
+              # Beginning/End of line
+              "C-a" = { with_mark = "home"; };
+              "C-e" = { with_mark = "end"; };
+              # Page up/down
+              "M-v" = { with_mark = "pageup"; };
+              "C-v" = { with_mark = "pagedown"; };
+              # Beginning/End of file
+              "M-Shift-comma" = { with_mark = "C-home"; };
+              "M-Shift-dot" = { with_mark = "C-end"; };
+              # Newline
+              "C-m" = "enter";
+              "C-j" = "enter";
+              "C-o" = [ "enter" "left" ];
+              # Copy
+              "C-w" = [ "C-x" { set_mark = false; } ];
+              "M-w" = [ "C-c" { set_mark = false; } ];
+              "C-y" = [ "C-v" { set_mark = false; } ];
+              # Delete
+              "C-d" = [ "delete" { set_mark = false; } ];
+              "M-d" = [ "C-delete" { set_mark = false; } ];
+              # Kill line
+              "C-k" = [ "Shift-end" "C-x" { set_mark = false; } ];
+              # Kill word backward
+              "Alt-backspace" = [ "C-backspace" { set_mark = false; } ];
+              # set mark next word continuously.
+              "C-M-space" = [ "C-Shift-right" { set_mark = true; } ];
+              # Undo
+              "C-slash" = [ "C-z" { set_mark = false; } ];
+              "C-Shift-ro" = "C-z";
+              # Mark
+              "C-space" = { set_mark = true; };
+              # Search
+              "C-s" = "C-f";
+              "C-r" = "Shift-F3";
+              "M-Shift-5" = "C-h";
+              # Cancel
+              "C-g" = [ "esc" { set_mark = false; } ];
+              # C-x YYY
+              "C-x" = {
+                remap = {
+                  # C-x h (select all)
+                  "h" = [ "C-home" "C-a" { set_mark = true; } ];
+                  # C-x C-f (open)
+                  "C-f" = "C-o";
+                  # C-x C-s (save)
+                  "C-s" = "C-s";
+                  # C-x k (kill tab)
+                  "k" = "C-f4";
+                  # C-x C-c (exit)
+                  "C-c" = "C-q";
+                  # C-x u (undo)
+                  "u" = [ "C-z" { set_mark = false; } ];
+                };
+              };
+            };
+          }
+        ];
+      };
     })
     (mkIf (cfg.enable && cfg.wm.enable) {
       wmCommon.keybindings.entries = [
