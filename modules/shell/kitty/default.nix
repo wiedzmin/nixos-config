f5dfa6a7d3bdbb5f7a4068cdde65f87d29225737
Alex Ermolov <aaermolov@gmail.com>
2024-08-02 22:04:40 +0300

    shell: kitty: set well-known TERM value
    Otherwise, ssh sessions become *very dumb* in the sense of commands editing.

--- modules/shell/kitty/default.nix
+++ modules/shell/kitty/default.nix
@@ -388,6 +388,7 @@ in
             mouse_map super+left press grabbed mouse_discard_event
             mouse_map super+left release grabbed,ungrabbed mouse_click_url
             mouse_map super+alt+left press ungrabbed mouse_selection rectangle
+            term xterm-256color
           '';
         };
       };
