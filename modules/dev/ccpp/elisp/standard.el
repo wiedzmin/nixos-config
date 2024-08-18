c484f5fce183a7eb27d8178532f15680877614ab
Alex Ermolov <aaermolov@gmail.com>
2024-08-19 01:00:59 +0300

    dev: ccpp: emacs: added forgotten clauses related to `lsp-mode`

--- modules/dev/ccpp/elisp/standard.el
+++ modules/dev/ccpp/elisp/standard.el
@@ -1,15 +1,21 @@
 (use-package c-mode
+  :after lsp-mode
   :mode (("\\.c$" . c-mode)
          ("\\.h$" . c-mode))
   ;; :after dap
+  :hook
+  ((c-mode-hook c++-mode-hook) . lsp-deferred)
   :config
   ;; (use-package dap-cpptools)
   (setq-default c-basic-offset 2))
 
 (use-package c++-mode
+  :after lsp-mode
   :mode (("\\.cpp$" . c++-mode)
          ("\\.h$" . c++-mode)
          ("\\.hpp$" . c++-mode))
+  :hook
+  ((c-mode-hook c++-mode-hook) . lsp-deferred)
   :config
   (setq-default c-basic-offset 2))
 
