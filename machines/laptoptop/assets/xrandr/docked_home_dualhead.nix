12b5b709ede6c4cdd13ca8fe1b3a09856976ea6b
Alex Ermolov <aaermolov@gmail.com>
2024-11-08 23:20:57 +0300

    laptoptop: assets: xrandr: dynamically maintain screen dimensions

--- machines/laptoptop/assets/xrandr/docked_home_dualhead.nix
+++ machines/laptoptop/assets/xrandr/docked_home_dualhead.nix
@@ -15,6 +15,7 @@ in
           mvWorkspacesCmdI3 config.wmCommon.workspaces "primary" config.attributes.hardware.monitors.externalPrimaryHead.name}${
           mvWorkspacesCmdI3 config.wmCommon.workspaces "secondary" config.attributes.hardware.monitors.externalSecondaryHead.name
         }${mvWorkspacesCmdI3 config.wmCommon.workspaces "tertiary" config.attributes.hardware.monitors.internalHead.name}"
+        ${pkgs.xorg.xdpyinfo}/bin/xdpyinfo | grep dimensions | cut -f2 -d: | tr -s ' ' | cut -d ' ' -f2 | tr -d '\n' | redis-cli -x set wm/dimensions
       '';
     };
   };
