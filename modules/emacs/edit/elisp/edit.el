86fbabcf68c9995dc16c7e9b171dedf5a831a892
Alex Ermolov <aaermolov@gmail.com>
2024-12-13 22:27:15 +0300

    emacs: edit: `expand-region` now has tree-sitter counterpart

--- modules/emacs/edit/elisp/edit.el
+++ modules/emacs/edit/elisp/edit.el
@@ -1,10 +1,3 @@
-(use-package expand-region
-  :bind
-  ("C-," . er/expand-region)
-  :config
-  (add-to-list 'expand-region-exclude-text-mode-expansions 'org-mode)
-  (add-to-list 'expand-region-exclude-text-mode-expansions 'LaTeX-mode))
-
 (use-package highlight-numbers
   :hook
   (foo-mode-hook . highlight-numbers-mode))
@@ -24,25 +17,6 @@
         ("}" . evilnc-copy-and-comment-lines)
         ("\\" . evilnc-comment-and-kill-ring-save)
         ("|" . evilnc-comment-or-uncomment-paragraphs)))
-
-(use-package easy-kill
-  :bind
-  ([remap kill-ring-save] . easy-kill)
-  ([remap mark-sexp] . easy-mark)
-  ([remap zap-to-char] . easy-mark-to-char)
-  (:map easy-kill-base-map
-        ("o" . easy-kill-er-expand)
-        ("i" . easy-kill-er-unexpand))
-  :config
-  (add-to-list 'easy-kill-alist '(?^ backward-line-edge ""))
-  (add-to-list 'easy-kill-alist '(?$ forward-line-edge ""))
-  (add-to-list 'easy-kill-alist '(?b buffer ""))
-  (add-to-list 'easy-kill-alist '(?< buffer-before-point ""))
-  (add-to-list 'easy-kill-alist '(?> buffer-after-point ""))
-  (add-to-list 'easy-kill-alist '(?f string-to-char-forward ""))
-  (add-to-list 'easy-kill-alist '(?F string-up-to-char-forward ""))
-  (add-to-list 'easy-kill-alist '(?t string-to-char-backward ""))
-  (add-to-list 'easy-kill-alist '(?T string-up-to-char-backward "")))
 
 (use-package easy-kill-extras
   :after easy-kill
