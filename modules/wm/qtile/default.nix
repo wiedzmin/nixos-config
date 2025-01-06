bff9565f406da9aae1a59e80aac74fddb8ef0923
Alex Ermolov <aaermolov@gmail.com>
2025-01-06 21:03:10 +0300

    wm: treewide: removed `stumpwm`
    - configuration is significantly outdated
    - no first-class IPC for profound system integration
    - workspaces are stretched to all monitors as a whole, e.g.
      when we swithcing workspaces, it changes on *all* monitors at once,
      and at the time of StumpWM usage (some years ago), there was no straightforward
      ability (if any at all) to make it another way, not talking about pinning
      workspaces to monitors
    
    Anyway it is kept forever in git history and could be resurrected on demand.

--- modules/wm/qtile/default.nix
+++ modules/wm/qtile/default.nix
@@ -43,7 +43,7 @@ in
     })
     (mkIf (cfg.enable && cfg.isDefault) {
       assertions = [{
-        assertion = !config.wm.awesome.isDefault && !config.wm.i3.isDefault && !config.wm.stumpwm.isDefault;
+        assertion = !config.wm.awesome.isDefault && !config.wm.i3.isDefault;
         message = "qtile: exactly one WM could be the default.";
       }];
 
