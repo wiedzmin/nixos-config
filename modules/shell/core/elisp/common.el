40cbe46f481104aee5186e3defbd81a3928062ae
Alex Ermolov <aaermolov@gmail.com>
2025-01-13 11:59:10 +0300

    shell: core: emacs: added `detached` for detached shell sessions

--- modules/shell/core/elisp/common.el
+++ modules/shell/core/elisp/common.el
@@ -1,3 +1,18 @@
+(use-package detached
+  :init
+  (use-package detached-init
+    :config
+    (detached-init))
+  :bind (;; Replace `async-shell-command' with `detached-shell-command'
+         ([remap async-shell-command] . detached-shell-command)
+         ;; Replace `compile' with `detached-compile'
+         ([remap compile] . detached-compile)
+         ([remap recompile] . detached-compile-recompile)
+         ;; Replace built in completion of sessions with `consult'
+         ([remap detached-open-session] . detached-consult-session))
+  :custom ((detached-show-output-on-attach t)
+           (detached-terminal-data-command system-type)))
+
 (use-package flycheck-checkbashisms
   :hook (flycheck-mode-hook . flycheck-checkbashisms-setup))
 
