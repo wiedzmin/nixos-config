73d3d1930fa430366b0c745eb2c582c868e6e313
Alex Ermolov <aaermolov@gmail.com>
2025-02-27 21:35:52 +0300

    dev: navigation: projects: added bookmark

--- modules/dev/navigation/projects/default.nix
+++ modules/dev/navigation/projects/default.nix
@@ -111,6 +111,10 @@ in
           desc = "cachix/devenv options";
           remote.url = "https://devenv.sh/reference/options/";
         };
+        "projectile/docs" = {
+          desc = "Projectile docs";
+          remote.url = "https://docs.projectile.mx/projectile/index.html";
+        };
         "cachix/devenv/releases" = {
           desc = "cachix/devenv releases";
           remote.url = "https://github.com/cachix/devenv/releases";
