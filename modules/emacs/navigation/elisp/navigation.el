49547e146dd0ba4daf7b12f6aaa5e3ddea3a4cd8
Alex Ermolov <aaermolov@gmail.com>
2025-03-03 15:44:48 +0300

    emacs: navigation: reformatted groups definitions for `bufler`
    - removed comments, they introduced visual clutter, counterintuitively
      leading to worse readability. When in doubt, one can always refer to
      package documentation or source code
    - relocated more visually structured

--- modules/emacs/navigation/elisp/navigation.el
+++ modules/emacs/navigation/elisp/navigation.el
@@ -542,65 +542,31 @@
   ("<f12>" . bufler-list)
   :config
   (bufler-defgroups
-  (group
-   ;; Subgroup collecting all named workspaces.
-   (auto-workspace))
-  (group
-   ;; Subgroup collecting all `help-mode' and `info-mode' buffers.
-   (group-or "*Help/Info*"
-             (mode-match "*Help*" (rx bos "help-"))
-             (mode-match "*Info*" (rx bos "info-"))))
-  (group
-   ;; Subgroup collecting all special buffers (i.e. ones that are not
-   ;; file-backed), except `magit-status-mode' buffers (which are allowed to fall
-   ;; through to other groups, so they end up grouped with their project buffers).
-   (group-and "*Special*"
-              (lambda (buffer)
-                (unless (or (funcall (mode-match "Magit" (rx bos "magit-status"))
-                                     buffer)
-                            (funcall (mode-match "Dired" (rx bos "dired"))
-                                     buffer)
-                            (funcall (auto-file) buffer))
-                  "*Special*")))
-   (group
-    ;; Subgroup collecting these "special special" buffers
-    ;; separately for convenience.
-    (name-match "**Special**"
-                (rx bos "*" (or "Messages" "Warnings" "scratch" "Backtrace") "*")))
-   (group
-    ;; Subgroup collecting all other Magit buffers, grouped by directory.
-    (mode-match "*Magit* (non-status)" (rx bos (or "magit" "forge") "-"))
-    (auto-directory))
-   ;; Subgroup for Helm buffers.
-   (mode-match "*Helm*" (rx bos "helm-"))
-   ;; Remaining special buffers are grouped automatically by mode.
-   (auto-mode))
-  ;; All buffers under "~/.emacs.d" (or wherever it is).
-  (dir user-emacs-directory)
-  (group
-   ;; Subgroup collecting buffers in `org-directory' (or "~/org" if
-   ;; `org-directory' is not yet defined).
-   (dir (if (bound-and-true-p org-directory)
-            org-directory
-          "~/org"))
-   (group
-    ;; Subgroup collecting indirect Org buffers, grouping them by file.
-    ;; This is very useful when used with `org-tree-to-indirect-buffer'.
-    (auto-indirect)
-    (auto-file))
-   ;; Group remaining buffers by whether they're file backed, then by mode.
-   (group-not "*special*" (auto-file))
-   (auto-mode))
-  (group
-   ;; Subgroup collecting buffers in a projectile project.
-   (auto-projectile))
-  (group
-   ;; Subgroup collecting buffers in a version-control project,
-   ;; grouping them by directory.
-   (auto-project))
-  ;; Group remaining buffers by directory, then major mode.
-  (auto-directory)
-  (auto-mode)))
+    (group (auto-workspace))
+    (group (group-or "*Help/Info*"
+                     (mode-match "*Help*" (rx bos "help-"))
+                     (mode-match "*Info*" (rx bos "info-"))))
+    (group
+     (group-and "*Special*"
+                (lambda (buffer)
+                  (unless (or (funcall (mode-match "Magit" (rx bos "magit-status")) buffer)
+                              (funcall (mode-match "Dired" (rx bos "dired")) buffer)
+                              (funcall (auto-file) buffer))
+                    "*Special*")))
+     (group (name-match "**Special**" (rx bos "*" (or "Messages" "Warnings" "scratch" "Backtrace") "*")))
+     (group (mode-match "*Magit* (non-status)" (rx bos (or "magit" "forge") "-")) (auto-directory))
+     (mode-match "*Helm*" (rx bos "helm-"))
+     (auto-mode))
+    (dir user-emacs-directory)
+    (group
+     (dir (if (bound-and-true-p org-directory) org-directory "~/org"))
+     (group (auto-indirect) (auto-file))
+     (group-not "*special*" (auto-file))
+     (auto-mode))
+    (group (auto-projectile))
+    (group (auto-project))
+    (auto-directory)
+    (auto-mode)))
 
 (use-package burly
   ;;TODO: also lift frames bookmark opening up to `open-project' script
