bbc5e8c7efc302ca967e4b4b0df86164a5ee3120
Alex Ermolov <aaermolov@gmail.com>
2025-03-04 21:42:15 +0300

    emacs: navigation: consult: project: little keybindings refactoring

--- modules/emacs/navigation/elisp/consult-project.el
+++ modules/emacs/navigation/elisp/consult-project.el
@@ -1,8 +1,5 @@
-(with-eval-after-load 'consult
-  (with-eval-after-load 'project
-    (keymap-set custom-projects-map "h" 'consult-project-extra-find))
-  (keymap-set custom-projects-map "r" 'consult-recent-file))
-
 (use-package consult-project-extra
+  :after project
   :bind
-  ("C-<f1>" . consult-project-extra-find))
+  (:map custom-projects-map
+        ("h" . consult-project-extra-find)))
