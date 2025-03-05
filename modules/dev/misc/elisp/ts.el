184010fb540511b674c66a89063fc84ebd4aeec5
Alex Ermolov <aaermolov@gmail.com>
2025-03-05 17:22:44 +0300

    treewide: json support refactoring

--- modules/dev/misc/elisp/ts.el
+++ modules/dev/misc/elisp/ts.el
@@ -6,3 +6,9 @@
 (use-package lua-ts-mode
   :mode
   ("\\lua\\'" . lua-ts-mode))
+
+(use-package json-ts-mode
+  :mode "\\.json$"
+  :hook
+  (json-ts-mode-hook . @lspStartFunctionJson@)
+  @lspModeJsonTSRegisterServer@)
