947c619e1db84f806ac2852b22a421cff59331ce
Alex Ermolov <aaermolov@gmail.com>
2025-03-10 10:52:02 +0300

    emacs: navigation: added prompters toggling for `embark`
    When someone knows exact keybindings for various `embark` tasks,
    it is more practical to issue them without any lookup (and implied
    time and mind capacity loss). OTOH, when someone need or want to
    discover some new tasks/keybindings, no matter the cause, it is
    undoubtedly convenient to have the ability to lookup and overview
    them.

--- modules/emacs/navigation/elisp/navigation.el
+++ modules/emacs/navigation/elisp/navigation.el
@@ -199,11 +199,18 @@
 
 (use-package embark
   :demand t
+  :preface
+  (defun custom/embark-toggle-prompter ()
+    (interactive)
+    (if (eq embark-prompter 'embark-completing-read-prompter)
+        (setq embark-prompter 'embark-keymap-prompter)
+      (setq embark-prompter 'embark-completing-read-prompter)))
   :bind
   ("C-S-a" . embark-act)
   ("C-*" . embark-act-all)
   ("C-S-d" . embark-dwim)
   ("C->" . embark-select)
+  ("C-&" . custom/embark-toggle-prompter)
   (:map embark-general-map
         ("C-." . embark-cycle))
   (:map mode-specific-map
@@ -224,7 +231,6 @@
   :custom
   (embark-allow-edit-default t)
   (embark-indicators '(embark-minimal-indicator embark-highlight-indicator))
-  (embark-prompter 'embark-completing-read-prompter)
   (prefix-help-command 'embark-prefix-help-command))
 
 (use-package marginalia
