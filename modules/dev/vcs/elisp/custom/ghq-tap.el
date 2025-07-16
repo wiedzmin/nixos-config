2a4024e18aa8310b61b1f36d404aa89406a2b8a8
Alex Ermolov <aaermolov@gmail.com>
2025-07-16 19:13:58 +0300

    dev: vcs: emacs: refined regexp for `ghq-thing-at-point`

--- modules/dev/vcs/elisp/custom/ghq-tap.el
+++ modules/dev/vcs/elisp/custom/ghq-tap.el
@@ -5,7 +5,7 @@
   (save-excursion
     (skip-chars-forward "[:alnum:]-:_/.#")
     (skip-chars-backward "[:alnum:]-:_/.#")
-    (if (looking-at "ghq#\\([a-z0-9\-\:\/\.\_]+\\)")
+    (if (looking-at "ghq#\\([a-z0-9\-\~\:\/\.\_]+\\)")
         (cons (+ (point) 4) (match-end 0))
       nil)))
 
