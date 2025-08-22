c07a974968a6e9a284abcb734d0aba8fd660e2ef
Alex Ermolov <aaermolov@gmail.com>
2025-08-22 23:13:45 +0300

    machines: laptoptop: bumped kernel version
    6.14 reached EOL upstream.

--- machines/laptoptop/default.nix
+++ machines/laptoptop/default.nix
@@ -77,7 +77,7 @@ in
     };
     initrd.availableKernelModules = [ "ahci" "ehci_pci" "sdhci_pci" "usb_storage" "xhci_pci" ];
     tmp.useTmpfs = false;
-    kernelPackages = pkgs.linuxPackages_6_14;
+    kernelPackages = pkgs.linuxPackages_6_16;
     supportedFilesystems = [ "ntfs" ];
   };
 
