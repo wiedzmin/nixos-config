53681b721e5419ab3e2ba71005b83bbefdf32636
Alex Ermolov <aaermolov@gmail.com>
2025-10-10 12:01:38 +0300

    pim: core: emacs: added handy `hyperbole` keybindings for `ace-window`

--- modules/pim/core/elisp/pim.el
+++ modules/pim/core/elisp/pim.el
@@ -12,3 +12,10 @@
   (remove-hook 'hyrolo-add-hook #'hyrolo-set-date)
   (hkey-ace-window-setup)
   (hyperbole-mode 1))
+
+(with-eval-after-load 'hyperbole
+  (with-eval-after-load 'ace-window
+    (push '(?l hkey-window-link "Hyperbole: Window Link") aw-dispatch-alist)
+    (push '(?t hkey-throw   "Hyperbole: Throw") aw-dispatch-alist)
+    (push '(?r hkey-replace "Hyperbole: Replace Here") aw-dispatch-alist)
+    (push '(?i hkey-drag-item "Hyperbole: Drag Item") aw-dispatch-alist)))
