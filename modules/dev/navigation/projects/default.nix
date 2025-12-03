a10da40c1ba473a20a8dc9bdd186b0171b8fc7be
Alex Ermolov <aaermolov@gmail.com>
2025-12-03 13:58:09 +0300

    navigation: projects: nix: added substituter for `devenv.cachix.org`

--- modules/dev/navigation/projects/default.nix
+++ modules/dev/navigation/projects/default.nix
@@ -70,6 +70,16 @@ in
           };
         };
       };
+      nix = {
+        settings = {
+          substituters = [
+            "https://devenv.cachix.org"
+          ];
+          trusted-public-keys = [
+            "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          ];
+        };
+      };
     })
     (mkIf (cfg.enable && cfg.emacs.enable) {
       ide.emacs.core.extraPackages = epkgs: [
