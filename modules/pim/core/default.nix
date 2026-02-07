c3f64b9ed0fec19891b37ffda68590dcaaaca61b
Alex Ermolov <aaermolov@gmail.com>
2026-02-07 21:50:14 +0300

    treewide: refined Obsidian bookmarks

--- modules/pim/core/default.nix
+++ modules/pim/core/default.nix
@@ -59,6 +59,114 @@ in
           url = "https://www.gnu.org/software/hyperbole/man/hyperbole.html";
           browseWith = appCmdFull config.attributes.browser.default.traits;
         };
+        obsidian-vaults-Archive = {
+          tags = [ "obsidian" "archive" ];
+          path = homePrefix user "docs/obsidian/Archive";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "Archive obsidian";
+              desktop = "sandbox";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-Common = {
+          tags = [ "obsidian" "common" ];
+          path = homePrefix user "docs/obsidian/Common";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "Common obsidian";
+              desktop = "sandbox";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-Culture = {
+          tags = [ "obsidian" "culture" ];
+          path = homePrefix user "docs/obsidian/Culture";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "Culture obsidian";
+              desktop = "var";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-Emacs = {
+          tags = [ "obsidian" "emacs" ];
+          path = homePrefix user "docs/obsidian/Emacs";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "Emacs obsidian";
+              desktop = "main";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-Housekeeping = {
+          tags = [ "obsidian" "housekeeping" ];
+          path = homePrefix user "docs/obsidian/Housekeeping";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "Housekeeping obsidian";
+              desktop = "main";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-NixOS = {
+          tags = [ "obsidian" "nixos" ];
+          path = homePrefix user "docs/obsidian/NixOS";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "NixOS obsidian";
+              desktop = "main";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-Problems = {
+          tags = [ "obsidian" "problems" ];
+          path = homePrefix user "docs/obsidian/Problems";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "Problems obsidian";
+              desktop = "sandbox";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-Tech = {
+          tags = [ "obsidian" "tech" ];
+          path = homePrefix user "docs/obsidian/Tech";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "Tech obsidian";
+              desktop = "main";
+              activate = true;
+            }
+          ];
+        };
+        obsidian-vaults-WorkEnv = {
+          tags = [ "obsidian" ];
+          path = homePrefix user "docs/obsidian/WorkEnv";
+          windowRules = [
+            {
+              class = "Emacs";
+              title = "WorkEnv obsidian";
+              desktop = "main";
+              activate = true;
+            }
+          ];
+        };
       };
     })
   ];
