9530e576221fe529d3d057e1d3307c23b073d7f6
Alex Ermolov <aaermolov@gmail.com>
2026-07-06 21:34:39 +0300

    content: core: updated inotify watches limit

--- modules/content/core/default.nix
+++ modules/content/core/default.nix
@@ -41,7 +41,7 @@ in
     (mkIf cfg.enable {
       boot.kernel.sysctl = {
         "fs.inotify.max_user_instances" = 1024;
-        "fs.inotify.max_user_watches" = 1048576;
+        "fs.inotify.max_user_watches" = 2097152;
         "fs.inotify.max_queued_events" = 32768;
       };
 
