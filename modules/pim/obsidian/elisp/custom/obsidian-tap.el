4a9f3fa34cbf162d80ae91c6a5a18f366369b7a4
Alex Ermolov <aaermolov@gmail.com>
2026-08-21 00:13:27 +0300

    pim: obsidian: hyperbole: added new buttons

--- modules/pim/obsidian/elisp/custom/obsidian-tap.el
+++ modules/pim/obsidian/elisp/custom/obsidian-tap.el
@@ -11,3 +11,27 @@
 
 (put 'obsidian-plugin 'bounds-of-thing-at-point
      'search-obsidian-plugin-thing-at-point)
+
+(defun list-obsidian-vault-plugins-thing-at-point ()
+  "`thing-at-point' entity for listing community plugins for specific vault."
+  (save-excursion
+    (skip-chars-forward "[:alnum:]-_>")
+    (skip-chars-backward "[:alnum:]-_>")
+    (if (looking-at "cplugs-->\\([a-z0-9\-]+\\)")
+        (cons (+ (point) 9) (match-end 0))
+      nil)))
+
+(put 'obsidian-vault-plugins 'bounds-of-thing-at-point
+     'list-obsidian-vault-plugins-thing-at-point)
+
+(defun list-obsidian-vault-plugins-data-thing-at-point ()
+  "`thing-at-point' entity for listing community plugins with custom settings for specific vault."
+  (save-excursion
+    (skip-chars-forward "[:alnum:]-_>")
+    (skip-chars-backward "[:alnum:]-_>")
+    (if (looking-at "cplugs-data-->\\([a-z0-9\-]+\\)")
+        (cons (+ (point) 14) (match-end 0))
+      nil)))
+
+(put 'obsidian-vault-plugins-data 'bounds-of-thing-at-point
+     'list-obsidian-vault-plugins-data-thing-at-point)
--- modules/pim/obsidian/elisp/navigation.el
+++ modules/pim/obsidian/elisp/navigation.el
@@ -1,6 +1,5 @@
 (with-eval-after-load 'hyperbole
   (use-package obsidian-tap)
-  (require 'rg)
 
   (defun obsidian/search-community-plugins (token)
     "Find and list all vaults for which plugin is installed."
@@ -11,4 +10,30 @@
     (let ((plugin (thing-at-point 'obsidian-plugin)))
       (when plugin
         (ibut:label-set plugin)
-        (hact 'obsidian/search-community-plugins plugin)))))
+        (hact 'obsidian/search-community-plugins plugin))))
+
+  (defun obsidian/list-community-plugins (vault)
+    "Find and list all community plugins for specific vault."
+    (find-dired-with-command
+     (format "@obsidianVaultsRoot@/%s/.obsidian/plugins/" vault)
+     "find . -maxdepth 1 -ls"))
+
+  (defib custom/hypb/obsidian/search-community-plugins ()
+    "Finds and lists all vaults for which plugin is installed."
+    (let ((vault (thing-at-point 'obsidian-vault-plugins)))
+      (when vault
+        (ibut:label-set vault)
+        (hact 'obsidian/list-community-plugins vault))))
+
+  (defun obsidian/list-community-plugins-data (vault)
+    "Find and list all community plugins for specific vault, with custom settings."
+    (find-dired-with-command
+     (format "@obsidianVaultsRoot@/%s/.obsidian/plugins" vault)
+     "find . -name \"data.json\" -ls"))
+
+  (defib custom/hypb/obsidian/search-community-plugins-data ()
+    "Finds and lists all vaults for which plugin is installed."
+    (let ((vault (thing-at-point 'obsidian-vault-plugins-data)))
+      (when vault
+        (ibut:label-set vault)
+        (hact 'obsidian/list-community-plugins-data vault)))))
